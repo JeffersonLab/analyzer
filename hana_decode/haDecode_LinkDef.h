45b05133cf1597182b8407d00d3863afa8875b4a
Ole Hansen <ole@jlab.org>
2002-04-01 21:28:52 +0000

    Restore THaEtClient commented out by mistake.

--- hana_decode/haDecode_LinkDef.h
+++ hana_decode/haDecode_LinkDef.h
@@ -8,7 +8,7 @@
 #pragma link C++ class THaCodaFile;
 #pragma link C++ class THaCrateMap;
 #pragma link C++ class THaEpicsStack;
-//#pragma link C++ class THaEtClient;
+#pragma link C++ class THaEtClient;
 #pragma link C++ class THaEvData;
 #pragma link C++ class THaFastBusWord;
 #pragma link C++ class THaHelicity;
