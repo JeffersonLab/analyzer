61c1b070217f4e86d3a1cb0a55718c0b9e7ce60a
Ole Hansen <ole@jlab.org>
2002-04-18 18:42:42 +0000

    Initial check-in.

---
+++ src/THaCluster.C
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+// THaCluster                                                                //
+//                                                                           //
+// A generic cluster.                                                        //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#include "THaCluster.h"
+#include <iostream>
+
+ClassImp(THaCluster)
+
+//_____________________________________________________________________________
+THaCluster& THaCluster::operator=( const THaCluster& rhs )
+{
+  // Assignment operator
+
+  TObject::operator=( rhs );
+  if( this != &rhs ) {
+    fCenter     = rhs.fCenter;
+  }
+  return *this;
+}
+
+//_____________________________________________________________________________
+void THaCluster::Clear( const Option_t* opt )
+{
+  // Clear the contents of the cluster
+
+  fCenter.SetXYZ( 0.0, 0.0, 0.0 );
+}
+
+//_____________________________________________________________________________
+void THaCluster::Print( Option_t* opt ) const
+{
+  // Print contents of cluster
+
+  cout << fCenter.X() << " " << fCenter.Y() << " " << fCenter.Z() << endl;
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
---
+++ src/THaCluster.h
@@ -0,0 +1,41 @@
+#ifndef ROOT_THaCluster
+#define ROOT_THaCluster
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+// THaCluster                                                                //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#include "TObject.h"
+#include "TVector3.h"
+
+class THaCluster : public TObject {
+
+public:
+  THaCluster() {}
+  THaCluster( const THaCluster& rhs ) : 
+    TObject(rhs), fCenter(rhs.fCenter) {}
+  THaCluster& operator=( const THaCluster& );
+  virtual ~THaCluster() {}
+
+  TVector3&        GetCenter()       { return fCenter; }
+  virtual void     SetCenter( Double_t x, Double_t y, Double_t z )
+  { fCenter.SetXYZ(x,y,z); }
+  virtual void     SetCenter( const TVector3& vec3 )
+  { fCenter = vec3; }
+
+  // TObject functions redefined
+  virtual void     Clear( Option_t* opt="" );
+  virtual void     Print( Option_t* opt="" ) const;
+
+protected:
+
+  TVector3         fCenter;          // Center coordinates of cluster
+
+  ClassDef(THaCluster,0)             // Generic wire chamber cluster
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif
