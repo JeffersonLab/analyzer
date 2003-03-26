d5fa9db2e156312a4a413a2e5c381ad6bafc240d
Ole Hansen <ole@jlab.org>
2003-03-26 02:27:38 +0000

    Add single/multi track flag and test functions.

--- src/THaPhysicsModule.h
+++ src/THaPhysicsModule.h
@@ -14,17 +14,22 @@ class THaPhysicsModule : public THaAnalysisObject {
 public:
   virtual ~THaPhysicsModule();
   
-  virtual Int_t        Process() = 0;
-  
+          bool  IsSingleTrack() const { return fSingleTrk; }
+          bool  IsMultiTrack()  const { return !IsSingleTrack(); }
+
+  virtual Int_t Process() = 0;
+
 protected:
 
   //Only derived classes may construct me
-  THaPhysicsModule() {}
+  THaPhysicsModule() : fSingleTrk(false) {}
   THaPhysicsModule( const char* name, const char* description );
   THaPhysicsModule( const THaPhysicsModule& ) {};
   THaPhysicsModule& operator=( const THaPhysicsModule& ) { return *this; }
 
   virtual void MakePrefix() { THaAnalysisObject::MakePrefix( NULL ); }
+
+  bool  fSingleTrk;              //Flag for single track mode
 
   ClassDef(THaPhysicsModule,0)   //ABC for a physics/kinematics module
 
