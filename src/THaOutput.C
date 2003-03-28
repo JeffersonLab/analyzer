82a56448c2dc67a889890281c1051ab43aa81c59
Ole Hansen <ole@jlab.org>
2003-03-28 21:22:44 +0000

    Merge cleaned-up code from Release-070 branch. Protect against re-initialization. No longer die if output.def cannot be found, but act as a dummy. Replace keyword map with more efficient structure. Remove several static member variables in favor of enum.

--- src/THaOutput.C
+++ src/THaOutput.C
@@ -4,6 +4,13 @@
 //
 // THaOutput
 //
+// Defines the tree and histogram output for THaAnalyzer.  
+// This class reads a file 'output.def' (an example is in /examples)
+// to define which global variables, including arrays, and formulas
+// (THaFormula's), and histograms go to the ROOT output.
+//
+// author:  R. Michaels    Sept 2002
+//
 //////////////////////////////////////////////////////////////////////////
 
 #define CHECKOUT 1
@@ -20,25 +27,26 @@
 #include "TTree.h"
 #include "TFile.h"
 #include "TRegexp.h"
-
 #include <algorithm>
-using namespace std;
 
-const Int_t THaOutput::fgVariden;
-const Int_t THaOutput::fgFormiden;
-const Int_t THaOutput::fgTh1fiden;
-const Int_t THaOutput::fgTh2fiden;
-const Int_t THaOutput::fgBlockiden;
+using namespace std;
+typedef vector<THaOdata*>::size_type Vsiz;
 
 //_____________________________________________________________________________
-THaOutput::THaOutput() 
+THaOutput::THaOutput() :
+  fNform(0), fNvar(0), fN1d(0), fN2d(0),
+  fForm(0), fVar(0), fH1vtype(0), fH1form(0), fH2vtypex(0), fH2formx(0),
+  fH2vtypey(0), fH2formy(0), fTree(0), fInit(false)
 {
-  fTree = 0;
+  // Constructor
 }
 
 //_____________________________________________________________________________
 THaOutput::~THaOutput() 
 {
+  // Destructor
+
+  // FIXME: Does this really clean up everything?
   delete fTree;
   delete [] fForm;
   delete [] fVar;
@@ -53,30 +61,36 @@ THaOutput::~THaOutput()
 //_____________________________________________________________________________
 Int_t THaOutput::Init( ) 
 {
-  Int_t iform,ivar,ihist;
-  string tinfo,cname;
-  fNbout = 4000;  
-  fKeyint.clear();
-  fKeyint.insert( make_pair (THaString("variable"), fgVariden));
-  fKeyint.insert( make_pair (THaString("formula"), fgFormiden));
-  fKeyint.insert( make_pair (THaString("th1f"), fgTh1fiden));
-  fKeyint.insert( make_pair (THaString("th2f"), fgTh2fiden));
-  fKeyint.insert( make_pair (THaString("block"), fgBlockiden));
+  // Initialize output system. Required before anything can happen.
 
-  if (fTree == 0) fTree = new TTree("T","Hall A Analyzer Output DST");
+  // Do not reinitialize
+  if( fInit ) {
+    cout << "\nTHaOutput::Init: Info: THaOutput cannot be ";
+    cout << "re-initialized. Keeping existing definitions." << endl;
+#ifdef CHECKOUT
+    Print();
+#endif
+    return 1;
+  }
+  if( !gHaVars ) return -2;
 
-  if ( !gHaVars ) return -2;
-  if ( LoadFile() != 0) return -3;
+  Int_t err = LoadFile();
+  if( err == -1 )
+    return 0;       // No error if file not found - then we're just a dummy
+  else if( err != 0 )
+    return -3;
 
   fNvar = fVarnames.size();  // this gets reassigned below
   fNform = fFormnames.size();
   fN1d = fH1dname.size();
   fN2d = fH2dname.size();
+
   fForm = new Double_t[fNform];
+  fTree = new TTree("T","Hall A Analyzer Output DST");
 
   THaVar *pvar;
   vector<string> stemp1,stemp2;
-  for (ivar = 0; ivar < fNvar; ivar++) {
+  for (Int_t ivar = 0; ivar < fNvar; ivar++) {
     pvar = gHaVars->Find(fVarnames[ivar].c_str());
     if (pvar) {
       if (pvar->IsArray()) {
@@ -93,43 +107,40 @@ Int_t THaOutput::Init( )
       cout << "There is probably a typo error... "<<endl;
     }
   }
-  for (Int_t k = 0; (unsigned long)k < fOdata.size(); k++) {
-     fTree->Branch(stemp1[k].c_str(), fOdata[k]->IsA()->GetName(),
-         &fOdata[k]);
-  } 
+  for (Vsiz k = 0; k < fOdata.size(); k++)
+    fTree->Branch(stemp1[k].c_str(), fOdata[k]->ClassName(),
+		  &fOdata[k]);
   fNvar = stemp2.size();
   fVar = new Double_t[fNvar];
-  for (Int_t k = 0; k < fNvar; k++) {
-    tinfo = stemp2[k]+"/D";
-    fTree->Branch(stemp2[k].c_str(), &fVar[k], tinfo.c_str(), fNbout);
-  }
-  for (iform = 0; iform < fNform; iform++) {
-    cname = Form("f%d",iform);
-    fFormulas.push_back(new THaFormula(cname.c_str(),fFormdef[iform].c_str()));
+  for (Int_t k = 0; k < fNvar; k++)
+    fTree->Branch(stemp2[k].c_str(), &fVar[k], 
+		  stemp2[k].append("/D").c_str(), kNbout);
+  for (Int_t iform = 0; iform < fNform; iform++) {
+    fFormulas.push_back(new THaFormula(Form("f%d",iform),
+				       fFormdef[iform].c_str()));
     if (fFormulas[iform]->IsError()) {
       cout << "THaOutput::Init: WARNING: Error in formula ";
       cout << fFormnames[iform] << endl;
       cout << "There is probably a typo error... " << endl;
     }
   }
-  for (iform = 0; iform < fNform; iform++) {
-    tinfo = fFormnames[iform] + "/D";
-    fTree->Branch(fFormnames[iform].c_str(), &fForm[iform], tinfo.c_str(), fNbout);   
-  }
+  for (Int_t iform = 0; iform < fNform; iform++)
+    fTree->Branch(fFormnames[iform].c_str(), &fForm[iform], 
+		  fFormnames[iform].append("/D").c_str(), kNbout);   
   fH1vtype = new Int_t[fN1d];
   fH1form  = new Int_t[fN1d];
   memset(fH1vtype, -1, fN1d*sizeof(Int_t));
   memset(fH1form, -1, fN1d*sizeof(Int_t));
-  for (ihist = 0; ihist < fN1d; ihist++) {
-    for (iform = 0; iform < fNform; iform++) {
+  for (Int_t ihist = 0; ihist < fN1d; ihist++) {
+    for (Int_t iform = 0; iform < fNform; iform++) {
       if (fH1plot[ihist] == fFormnames[iform]) {
-        fH1vtype[ihist] = fgFormiden;
+        fH1vtype[ihist] = kForm;
 	fH1form[ihist] = iform;  
       }
     } 
     pvar = gHaVars->Find(fH1plot[ihist].c_str());
-    if (pvar != 0 && fH1vtype[ihist] != fgFormiden) 
-        fH1vtype[ihist] = fgVariden;
+    if (pvar != 0 && fH1vtype[ihist] != kForm) 
+        fH1vtype[ihist] = kVar;
     fH1var.push_back(pvar);
     fH1d.push_back( new TH1F (fH1dname[ihist].c_str(), 
          fH1dtit[ihist].c_str(), 
@@ -143,30 +154,31 @@ Int_t THaOutput::Init( )
   memset(fH2vtypey, -1, fN2d*sizeof(Int_t));
   memset(fH2formx, -1, fN2d*sizeof(Int_t));
   memset(fH2formy, -1, fN2d*sizeof(Int_t));
-  for (ihist = 0; ihist < fN2d; ihist++) {
-    for (iform = 0; iform < fNform; iform++) {
+  for (Int_t ihist = 0; ihist < fN2d; ihist++) {
+    for (Int_t iform = 0; iform < fNform; iform++) {
       if (fH2plotx[ihist] == fFormnames[iform]) {
-        fH2vtypex[ihist] = fgFormiden;
+        fH2vtypex[ihist] = kForm;
 	fH2formx[ihist] = iform;  
       }
       if (fH2ploty[ihist] == fFormnames[iform]) {
-        fH2vtypey[ihist] = fgFormiden;
+        fH2vtypey[ihist] = kForm;
 	fH2formy[ihist] = iform;  
       }
     } 
     pvar = gHaVars->Find(fH2plotx[ihist].c_str());
-    if (pvar != 0 && fH2vtypex[ihist] != fgFormiden) 
-        fH2vtypex[ihist] = fgVariden;
+    if (pvar != 0 && fH2vtypex[ihist] != kForm) 
+        fH2vtypex[ihist] = kVar;
     fH2varx.push_back(pvar);
     pvar = gHaVars->Find(fH2ploty[ihist].c_str());
-    if (pvar != 0 && fH2vtypey[ihist] != fgFormiden) 
-        fH2vtypey[ihist] = fgVariden;
+    if (pvar != 0 && fH2vtypey[ihist] != kForm) 
+        fH2vtypey[ihist] = kVar;
     fH2vary.push_back(pvar);
     fH2d.push_back( new TH2F (fH2dname[ihist].c_str(), 
          fH2dtit[ihist].c_str(), 
 	 fH2dbinx[ihist], fH2dxlo[ihist], fH2dxhi[ihist],
 	 fH2dbiny[ihist], fH2dylo[ihist], fH2dyhi[ihist]));
   }
+  fInit = true;
   return 0;
 }
  
@@ -180,7 +192,7 @@ Int_t THaOutput::AddToTree(char *name, TObject *tobj)
   // Instead, it works to add a TObject like this for THaOutput *fOut :
   // if (fOut->TreeDefined()) fOut->GetTree()->Branch(...etc)
   if (fTree != 0 && tobj != 0) {
-    fTree->Branch(name,tobj->IsA()->GetName(),&tobj);
+    fTree->Branch(name,tobj->ClassName(),&tobj);
     return 0;
   }
   return -1;
@@ -199,12 +211,11 @@ Int_t THaOutput::Process()
       }
   }
   THaVar *pvar;
-  unsigned long k;
   for (Int_t ivar = 0; ivar < fNvar; ivar++) {
     pvar = fVariables[ivar];
     if (pvar) fVar[ivar] = pvar->GetValue();
   }
-  for (k = 0; k < fOdata.size(); k++) { 
+  for (Vsiz k = 0; k < fOdata.size(); k++) { 
     fOdata[k]->Clear();
     pvar = fArrays[k];
     if ( pvar == 0) continue;
@@ -217,7 +228,7 @@ Int_t THaOutput::Process()
     }
   }
   for (Int_t ihist = 0; ihist < fN1d; ihist++) {
-    if (fH1vtype[ihist] == fgFormiden) {
+    if (fH1vtype[ihist] == kForm) {
       if (fH1form[ihist] >= 0) {
         fH1d[ihist]->Fill(fForm[fH1form[ihist]]);
       }
@@ -236,13 +247,14 @@ Int_t THaOutput::Process()
   }
   for (Int_t ihist = 0; ihist < fN2d; ihist++) {
     Float_t x = 0;  Float_t y = 0;
-    if (fH2vtypex[ihist] == fgFormiden) {
+    if (fH2vtypex[ihist] == kForm) {
       if (fH2formx[ihist] >= 0) {
         x = fForm[fH2formx[ihist]];
       }
     } else {
       pvar = fH2varx[ihist];    
       if (pvar) {
+	// FIXME: Put this check into Init()?
         if (pvar->IsArray()) {
           cout << "WARNING: 2d histos w/ arrays not supported"<<endl;
           x = 0; // cannot handle arrays yet
@@ -251,7 +263,7 @@ Int_t THaOutput::Process()
         }
       }
     }
-    if (fH2vtypey[ihist] == fgFormiden) {
+    if (fH2vtypey[ihist] == kForm) {
       if (fH2formy[ihist] >= 0) {
         y = fForm[fH2formy[ihist]];
       }
@@ -286,9 +298,8 @@ Int_t THaOutput::End()
 Int_t THaOutput::LoadFile() 
 {
   // Process the file that defines the output
-  ifstream *odef;
-  THaString loadfile = "output.def";
-  odef = new ifstream(loadfile.c_str());
+  const THaString loadfile = "output.def";
+  ifstream* odef = new ifstream(loadfile.c_str());
   if ( ! (*odef) ) {
     ErrFile(-1, loadfile);
     return -1;
@@ -313,13 +324,12 @@ Int_t THaOutput::LoadFile()
       ErrFile(0, sline);
       continue;
     }
-    Int_t status;
     Int_t ikey = FindKey(strvect[0]);
     switch (ikey) {
-      case fgVariden:
+      case kVar:
   	  fVarnames.push_back(strvect[1]);
           break;
-      case fgFormiden:
+      case kForm:
           if (strvect.size() < 3) {
 	    ErrFile(ikey, sline);
             continue;
@@ -327,9 +337,8 @@ Int_t THaOutput::LoadFile()
           fFormnames.push_back(strvect[1]);
           fFormdef.push_back(strvect[2]);
           break;
-      case fgTh1fiden:
-  	  status = ParseTitle(sline);
-          if (status != 1) {
+      case kH1:
+          if( ParseTitle(sline) != 1) {
 	    ErrFile(ikey, sline);
             continue;
 	  }
@@ -340,12 +349,11 @@ Int_t THaOutput::LoadFile()
 	  fH1dxlo.push_back(xl1);
 	  fH1dxhi.push_back(xh1);
           break;    
-      case fgTh2fiden:
-  	  status = ParseTitle(sline);
-          if (status != 2) {
-	    ErrFile(ikey, sline);
-            continue;
-	  }
+      case kH2:
+	if( ParseTitle(sline) != 2 ) {
+	  ErrFile(ikey, sline);
+	  continue;
+	}
 	  fH2dname.push_back(strvect[1]);
           fH2dtit.push_back(stitle);  
           fH2plotx.push_back(sfvar1);  
@@ -357,8 +365,8 @@ Int_t THaOutput::LoadFile()
 	  fH2dylo.push_back(xl2);
 	  fH2dyhi.push_back(xh2);
           break;
-      case fgBlockiden:
-	  status = BuildBlock(strvect[1]);
+      case kBlock:
+	  BuildBlock(strvect[1]);
 	  break;
       default:
         cout << "Warning: keyword "<<strvect[0]<<" undefined "<<endl;
@@ -384,18 +392,37 @@ Int_t THaOutput::LoadFile()
 }
 
 //_____________________________________________________________________________
-Int_t THaOutput::FindKey(THaString key) 
+Int_t THaOutput::FindKey(const THaString& key) const
 {
   // Return integer flag corresponding to
   // case-insensitive keyword "key" if it exists
-    map<THaString, Int_t >::iterator dmap = 
-        fKeyint.find(THaString(key).ToLower());
-    if (dmap != fKeyint.end()) return dmap->second;
-    return -1;
+  
+  // Map of keywords to internal logical type numbers
+  struct KeyMap {
+    const char* name;
+    EId keyval;
+  };
+  static const KeyMap keymap[] = { 
+    { "variable", kVar },
+    { "formula",  kForm },
+    { "th1f",     kH1 },
+    { "th2f",     kH2 },
+    { "block",    kBlock },
+    { 0 }
+  };
+
+  if( const KeyMap* it = keymap ) {
+    while( it->name ) {
+      if( key.CmpNoCase( it->name ) == 0 )
+	return it->keyval;
+      it++;
+    }
+  }
+  return -1;
 }
 
 //_____________________________________________________________________________
-void THaOutput::ErrFile(Int_t iden, THaString sline) 
+void THaOutput::ErrFile(Int_t iden, const THaString& sline) const
 {
   // Print error messages about the output definition file.
   if (iden == -1) {
@@ -409,27 +436,24 @@ void THaOutput::ErrFile(Int_t iden, THaString sline)
   cerr << "THaOutput::ERROR: Syntax error in output definition file."<<endl;
   cerr << "The offending line is :\n"<<sline<<endl<<endl;
   switch (iden) {
-     case 0:
-       cerr << "See the documentation or ask Bob Michaels"<<endl;
-       break;
-     case fgVariden:
+     case kVar:
        cerr << "For variables, the syntax is: "<<endl;
        cerr << "    variable  variable-name"<<endl;
        cerr << "Example: "<<endl;
        cerr << "    variable   R.vdc.v2.nclust"<<endl;;
-     case fgFormiden:
+     case kForm:
        cerr << "For formulas, the syntax is: "<<endl;
        cerr << "    formula  formula-name  formula-expression"<<endl;
        cerr << "Example: "<<endl;
        cerr << "    formula targetX 1.464*B.bpm4b.x-0.464*B.bpm4a.x"<<endl;
-     case fgTh1fiden:
+     case kH1:
        cerr << "For 1D histograms, the syntax is: "<<endl;
        cerr << "  TH1F name  'title'  variable nbin xlo xhi"<<endl;
        cerr << "Example: "<<endl;
        cerr << "  TH1F  tgtx 'target X' targetX 100 -2 2"<<endl;
        cerr << "(Title in single quotes.  Variable can be a formula)"<<endl;
        break;
-     case fgTh2fiden:
+     case kH2:
        cerr << "For 2D histograms, the syntax is: "<<endl;
        cerr << "  TH2F  name  'title'  var1  var2";
        cerr << "  nbinx xlo xhi  nbiny ylo yhi"<<endl;
@@ -439,26 +463,27 @@ void THaOutput::ErrFile(Int_t iden, THaString sline)
        cerr << "(Title in single quotes.  Variable can be a formula)"<<endl;
        break;
      default:
+       cerr << "See the documentation or ask Bob Michaels"<<endl;
        break;
   }
 }
 
 //_____________________________________________________________________________
-void THaOutput::Print() 
+void THaOutput::Print() const
 {
   // Printout the definitions
+  typedef vector<const THaString>::iterator Iter;
   cout << "\n=== Number of variables "<<fVarnames.size()<<endl;
-  for (vector<THaString>::iterator is = fVarnames.begin();
+  for (Iter is = fVarnames.begin();
     is != fVarnames.end(); is++) cout << " Variable = "<<*is<<endl;
   cout << "\n=== Number of formulas "<<fFormnames.size()<<endl;
-  for (vector<THaString>::iterator is = fFormnames.begin();
+  for (Iter is = fFormnames.begin();
     is != fFormnames.end(); is++) cout << "Formula name = "<<*is<<endl;
   cout << "\n=== Formula definitions "<<fFormdef.size()<<endl;
-  for (vector<THaString>::iterator is = fFormdef.begin();
+  for (Iter is = fFormdef.begin();
     is != fFormdef.end(); is++) cout << "Formula definition = "<<*is<<endl;
   cout << "\n=== Number of 1d histograms "<<fH1dname.size()<<endl;
-  unsigned long i;
-  for (i = 0; i < fH1dname.size(); i++) {
+  for (Vsiz i = 0; i < fH1dname.size(); i++) {
     cout << "1d histo "<<i<<"   name "<<fH1dname[i];
     cout << "  title =  "<<fH1dtit[i]<<endl;
     cout << "   var = "<<fH1plot[i];
@@ -466,7 +491,7 @@ void THaOutput::Print()
     cout << "   "<<fH1dxhi[i]<<endl;
   }
   cout << "\n=== Number of 2d histograms "<<fH2dname.size()<<endl;
-  for (i = 0; i < fH2dname.size(); i++) {
+  for (Vsiz i = 0; i < fH2dname.size(); i++) {
     cout << "2d histo "<<i<<"   name "<<fH2dname[i]<<endl;
     cout << "  title =  "<<fH2dtit[i];
     cout << "  x var = "<<fH2plotx[i];
@@ -480,32 +505,29 @@ void THaOutput::Print()
 }
 
 //_____________________________________________________________________________
-Int_t THaOutput::ParseTitle(THaString sline) 
+Int_t THaOutput::ParseTitle(const THaString& sline)
 {
   // Parse the string that defines the histogram.  The title must be
   // enclosed in single quotes (e.g. 'my title').  Ret value 'result'
   // means:  -1 == error,  1 == ok 1D histogram,  2 == ok 2D histogram
   Int_t result = -1;
   stitle = "";   sfvar1 = "";  sfvar2  = "";
-  Int_t pos1, pos2;
-  pos1 = 0;  pos2 = 0;
-  THaString ctemp;
-  pos1 = sline.find_first_of("'");
-  pos2 = sline.find_last_of("'");
-  if (pos1 >= 0 && pos2 > pos1) {
+  THaString::size_type pos1 = sline.find_first_of("'");
+  THaString::size_type pos2 = sline.find_last_of("'");
+  if (pos1 != THaString::npos && pos2 > pos1) {
     stitle = sline.substr(pos1+1,pos2-pos1-1);
   }
-  ctemp = sline.substr(pos2+1,sline.size()-pos2);
+  THaString ctemp = sline.substr(pos2+1,sline.size()-pos2);
   vector<THaString> stemp = ctemp.Split();
-  if ((long)stemp.size() > 1) {
+  if (stemp.size() > 1) {
      sfvar1 = stemp[0];
-     if ((long)stemp.size() == 4) {
+     if (stemp.size() == 4) {
        sscanf(stemp[1].c_str(),"%d",&n1);
        sscanf(stemp[2].c_str(),"%f",&xl1);
        sscanf(stemp[3].c_str(),"%f",&xh1);
        result = 1;
      }
-     if ((long)stemp.size() == 8) {
+     if (stemp.size() == 8) {
        sfvar2 = stemp[1];
        sscanf(stemp[2].c_str(),"%d",&n1);
        sscanf(stemp[3].c_str(),"%f",&xl1);
@@ -520,7 +542,7 @@ Int_t THaOutput::ParseTitle(THaString sline)
 }
 
 //_____________________________________________________________________________
-Int_t THaOutput::BuildBlock(THaString blockn)
+Int_t THaOutput::BuildBlock(const THaString& blockn)
 {
   // From the block name, identify and save a specific grouping
   // of global variables by adding them to the fVarnames list.
--- src/THaOutput.h
+++ src/THaOutput.h
@@ -5,13 +5,6 @@
 //
 // THaOutput
 //
-// Defines the tree and histogram output for THaAnalyzer.  
-// This class reads a file 'output.def' (an example is in /examples)
-// to define which global variables, including arrays, and formulas
-// (THaFormula's), and histograms go to the ROOT output.
-//
-// author:  R. Michaels    Sept 2002
-//
 //////////////////////////////////////////////////////////////////////////
 
 #define THAOMAX 100
@@ -22,7 +15,6 @@
 #include "TTree.h"
 #include "TNamed.h"
 #include <vector>
-#include <map>
 #include <string>
 #include <fstream>
 #include <iostream>
@@ -36,40 +28,59 @@ class THaOdata : public TObject {
 // Utility class used by THaOutput to store arrays 
 // up to size 'nsize' for tree output.
 public:
-  THaOdata() { nsize = THAOMAX; data = new Double_t[THAOMAX]; Clear(); }
-  THaOdata(int n) : nsize(n) { data = new Double_t[n]; Clear(); }
+  THaOdata(int n=THAOMAX) : nsize(n) { data = new Double_t[n]; Clear(); }
+  THaOdata(const THaOdata& other) : nsize(other.nsize) {
+    data = new Double_t[nsize]; ndata = other.ndata;
+    memcpy( data, other.data, nsize*sizeof(Double_t));
+  }
+  THaOdata& operator=(const THaOdata& rhs) { 
+    if( this != &rhs ) {
+      if( nsize < rhs.nsize ) {
+	nsize = rhs.nsize; delete [] data; data = new Double_t[nsize];
+      }
+      ndata = rhs.ndata; memcpy( data, rhs.data, nsize*sizeof(Double_t));
+    }
+    return *this; 
+  }
   virtual ~THaOdata() { delete [] data; };
-  void Clear() { ndata = 0; memset(data, 0, nsize*sizeof(Double_t)); };
+  void Clear( Option_t* opt="" ) {
+    ndata = 0; memset(data, 0, nsize*sizeof(Double_t)); 
+  }
   Int_t Fill(Int_t i, Double_t dat) {
-     if (i >= 0 && i < nsize-1) {
-        if (i >= ndata) ndata = i+1;
-        data[i] = dat;
-        return 1;
-     }
-     return 0;
-  };
-  Int_t       ndata;
-  Int_t       nsize;     
-  Double_t*   data;    // [ndata] 
-private:
-  THaOdata(const THaOdata& odata) {}
-  THaOdata& operator=(const THaOdata& odata) { return *this; }
+    if (i >= 0 && i < nsize-1) {
+      if (i >= ndata) ndata = i+1;
+      data[i] = dat;
+      return 1;
+    }
+    return 0;
+  }
+  Int_t Fill(Int_t n, const Double_t* array) {
+    if( n<0 || n>nsize ) return 0;
+    memcpy( data, array, n*sizeof(Double_t));
+    ndata = n;
+    return 1;
+  }
+  Int_t       ndata;   // Number of array elements
+  Int_t       nsize;   //!Maximum number of elements
+  Double_t*   data;    // [ndata] Array data
 
-  ClassDef(THaOdata,2)  // Variable sized array
+private:
+
+  ClassDef(THaOdata,3)  // Variable sized array
 };
 
 class THaOutput {
   
 public:
 
-   THaOutput( );
-   virtual ~THaOutput(); 
+  THaOutput();
+  virtual ~THaOutput(); 
 
    virtual Int_t Init( );
    virtual Int_t Process( );
    virtual Int_t End();
-   virtual Bool_t TreeDefined() { return fTree != 0; };
-   virtual TTree* GetTree() { return fTree; };
+   virtual Bool_t TreeDefined() const { return fTree != 0; };
+   virtual TTree* GetTree() const { return fTree; };
 #ifdef IFCANWORK
 // Preferred method, but doesn't work, hence turned off with ifdef
    virtual Int_t AddToTree(char *name, TObject *tobj); 
@@ -78,44 +89,39 @@ public:
 protected:
 
    virtual Int_t LoadFile();
-   virtual Int_t FindKey(THaString key);
-   virtual void ErrFile(Int_t iden, THaString sline);
-   virtual Int_t ParseTitle(THaString sline);
-   virtual Int_t BuildBlock(THaString blockn);
-   
-   void Print();
+   virtual Int_t FindKey(const THaString& key) const;
+   virtual void ErrFile(Int_t iden, const THaString& sline) const;
+   virtual Int_t ParseTitle(const THaString& sline);
+   virtual Int_t BuildBlock(const THaString& blockn);
+   void Print() const;
 
    // Variables, Formulas, Histograms
 
-   std::vector<THaString> fVarnames;
-   std::vector<THaString> fFormnames, fFormdef;
-   std::vector<THaString> fH1dname, fH1dtit, fH2dname, fH2dtit;
-   std::vector<Int_t> fH1dbin, fH2dbinx, fH2dbiny;
-   std::vector<Double_t> fH1dxlo, fH1dxhi;
-   std::vector<Double_t> fH2dxlo, fH2dxhi, fH2dylo, fH2dyhi;
-   Int_t fNform, fNvar, fNbout, fN1d, fN2d;
-   std::vector<THaFormula* > fFormulas;
-   std::vector<THaVar* > fVariables, fArrays;
-   Double_t *fForm, *fVar;
-   std::vector<TH1F* > fH1d;
-   std::vector<TH2F* > fH2d;
-   std::vector<THaString> fH1plot, fH2plotx, fH2ploty;
-   std::vector<THaVar* > fH1var, fH2varx, fH2vary;
-   Int_t *fH1vtype, *fH1form;
-   Int_t *fH2vtypex, *fH2formx;
-   Int_t *fH2vtypey, *fH2formy;
-   std::vector<THaOdata* > fOdata;
-   TTree *fTree; 
+  std::vector<THaString> fVarnames;
+  std::vector<THaString> fFormnames, fFormdef;
+  std::vector<THaString> fH1dname, fH1dtit, fH2dname, fH2dtit;
+  std::vector<Int_t> fH1dbin, fH2dbinx, fH2dbiny;
+  std::vector<Double_t> fH1dxlo, fH1dxhi;
+  std::vector<Double_t> fH2dxlo, fH2dxhi, fH2dylo, fH2dyhi;
+  Int_t fNform, fNvar, fN1d, fN2d;
+  std::vector<THaFormula* > fFormulas;
+  std::vector<THaVar* > fVariables, fArrays;
+  Double_t *fForm, *fVar;
+  std::vector<TH1F* > fH1d;
+  std::vector<TH2F* > fH2d;
+  std::vector<THaString> fH1plot, fH2plotx, fH2ploty;
+  std::vector<THaVar* > fH1var, fH2varx, fH2vary;
+  Int_t *fH1vtype, *fH1form;
+  Int_t *fH2vtypex, *fH2formx;
+  Int_t *fH2vtypey, *fH2formy;
+  std::vector<THaOdata* > fOdata;
+  TTree *fTree; 
+  bool fInit;
 
-   std::map<THaString, Int_t> fKeyint;
+  enum EId { kVar = 1, kForm, kH1, kH2, kBlock };
+  static const Int_t kNbout = 4000;
 
 private:
-
-   static const Int_t fgVariden  = 1;
-   static const Int_t fgFormiden = 2;
-   static const Int_t fgTh1fiden = 3;
-   static const Int_t fgTh2fiden = 4;
-   static const Int_t fgBlockiden= 5;
 
    THaOutput(const THaOutput& output) {}
    THaOutput& operator=(const THaOutput& output) { return *this; }
