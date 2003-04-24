6bf82cdcc4ec5062d05884f781541345b9ebc1df
Bodo Reitz <reitz@jlab.org>
2003-04-24 19:27:48 +0000

    new class for generic beamline apparatus

--- src/THaBeam.C
+++ src/THaBeam.C
@@ -10,7 +10,41 @@
 //////////////////////////////////////////////////////////////////////////
 
 #include "THaBeam.h"
+#include "VarDef.h"
 
 ClassImp(THaBeam)
 
+
 //_____________________________________________________________________________
+THaBeam::THaBeam( const char* name, const char* desc ) : 
+  THaApparatus( name,desc )
+{
+  // Constructor.
+  // Protected. Can only be called by derived classes.
+
+
+  fDetectors    = new TList;
+}
+
+
+//_____________________________________________________________________________
+Int_t THaBeam::DefineVariables( EMode mode )
+{
+  // Initialize global variables and lookup table for decoder
+
+  if( mode == kDefine && fIsSetup ) return kOK;
+  fIsSetup = ( mode == kDefine );
+
+  RVarDef vars[] = {
+    { "x", "reconstructed x-position at nom. interaction point", "fPosition.fX"},
+    { "y", "reconstructed y-position at nom. interaction point", "fPosition.fY"},
+    { "z", "reconstructed z-position at nom. interaction point", "fPosition.fZ"},
+    { "dir.x", "reconstructed x-component of beam direction", "fDirection.fX"},
+    { "dir.y", "reconstructed y-component of beam direction", "fDirection.fY"},
+    { "dir.z", "reconstructed z-component of beam direction", "fDirection.fZ"},
+    { 0 }
+  };
+    
+  return DefineVarsFromList( vars, mode );
+
+}
