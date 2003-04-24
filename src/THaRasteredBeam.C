973552ea3b1ab8d370e375f0c84125ee206f661c
Bodo Reitz <reitz@jlab.org>
2003-04-24 19:25:38 +0000

    Apparatus for getting beampositions using the THaRaster class

---
+++ src/THaRasteredBeam.C
@@ -0,0 +1,62 @@
+//*-- Author :    Bodo Reitz April 2003
+
+//////////////////////////////////////////////////////////////////////////
+//
+// THaRasteredBeam
+//
+// Apparatus describing an rastered beam.
+// 
+//////////////////////////////////////////////////////////////////////////
+
+#include "THaRasteredBeam.h"
+#include "THaRaster.h"
+#include "TMath.h"
+#include "TDatime.h"
+#include "VarDef.h"
+
+
+ClassImp(THaRasteredBeam)
+
+//_____________________________________________________________________________
+
+THaRasteredBeam::THaRasteredBeam( const char* name, const char* description ) :
+    THaBeam( name, description ) 
+{
+  AddDetector( new THaRaster("Raster","raster",this) );
+}
+
+
+Int_t THaRasteredBeam::Reconstruct()
+{
+
+  TIter nextDet( fDetectors ); 
+
+  nextDet.Reset();
+
+  // This apparatus assumes that there is only one detector 
+  // in the list. If someone adds detectors by hand, the first 
+  // detector in the list will be used to get the beam position
+  // the others will be processed
+
+  
+
+  if (THaBeamDet* theBeamDet=
+      static_cast<THaBeamDet*>( nextDet() )) {
+    theBeamDet->Process();
+    fPosition = theBeamDet->GetPosition();
+    fDirection = theBeamDet->GetDirection();
+  }
+  else {
+    Error( Here("Reconstruct()"), "Beamline Detectors Missing in Detector List" );
+  }
+
+
+  // Process any other detectors that may have been added (by default none)
+  while (THaBeamDet * theBeamDet=
+	 static_cast<THaBeamDet*>( nextDet() )) {
+    theBeamDet->Process();
+  }
+
+  return 0;
+
+}
