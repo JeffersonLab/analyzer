bc40b638b8af844e61355520f795084fb23d5017
Rob Feuerbach <feuerbac@jlab.org>
2003-10-17 00:53:36 +0000

    Merge from rob_db. New generic database API (THaDB class) and a file-based implementation (THaDBFile). See THaScintillator for a partial example of how to use the API.

---
+++ src/THaDB.h
@@ -0,0 +1,150 @@
+#ifndef HallA_THaDB
+#define HallA_THaDB
+
+//////////////////////////////////////////////////////////////////////////
+//
+// THaDB
+//
+// Simple abstract class to define interface for Database access.
+//   Mostly general, but with specialized members for dealing with the
+//   detector maps.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "TObject.h"
+#include "TDatime.h"
+
+#include "VarDef.h"
+#include "VarType.h"
+
+#include<string>
+#include<vector>
+
+class THaDetMap;
+
+class THaDB;
+class THaDetConfig;
+
+extern THaDB* gHaDB;
+
+
+
+class THaDB : public TObject {
+
+ public:
+  THaDB();
+  
+  virtual ~THaDB();
+
+  // For all 'Get' routines, the returned Int_t is the number of elements read
+  // and should be >0 for success.
+
+  virtual Int_t GetValue( const char* system, const char* attr,
+			  Int_t& value, const TDatime &date ) = 0;
+  virtual Int_t GetValue( const char* system, const char* attr,
+			  Double_t& value, const TDatime &date ) = 0;
+  virtual Int_t GetValue( const char* system, const char* attr,
+			  std::string& value, const TDatime &date ) = 0;
+  
+  virtual Int_t GetArray( const char* system, const char* attr,
+			  std::vector<Int_t>& array, const TDatime &date ) = 0;
+  virtual Int_t GetArray( const char* system, const char* attr,
+			  std::vector<Double_t>& array, const TDatime &date ) = 0;
+  
+  virtual Int_t GetArray( const char* system, const char* attr,
+			  Int_t* array, Int_t size, const TDatime &date ) = 0;
+  virtual Int_t GetArray( const char* system, const char* attr,
+			  Double_t* array, Int_t size, const TDatime &date ) = 0;
+  
+  virtual Int_t LoadValues ( const char* system, const TagDef* list,
+			     const TDatime& date ) = 0;
+
+  virtual Int_t GetMatrix( const char* system, const char* name,
+			   std::vector<std::vector<Int_t> >& rows,
+			   const TDatime& date ) = 0;
+  virtual Int_t GetMatrix( const char* system, const char* name,
+			   std::vector<std::vector<Double_t> >& rows,
+			   const TDatime& date ) = 0;
+  
+  // specialized for elements with named rows -- TRANSPORT MATRIX
+  virtual Int_t GetMatrix( const char* system,
+			   std::vector<std::string>& row_name,
+			   std::vector<std::vector<Double_t> >& row_data,
+			   const TDatime& date ) = 0;
+  
+  virtual Int_t PutValue( const char* system, const char* attr,
+			  const Int_t& value, const TDatime& date ) = 0;
+  virtual Int_t PutValue( const char* system, const char* attr,
+			  const Double_t& value, const TDatime& date ) = 0;
+  virtual Int_t PutValue( const char* system, const char* attr,
+			  const std::string& value, const TDatime& date ) = 0;
+  
+  virtual Int_t PutArray( const char* system, const char* attr,
+			  const std::vector<Int_t>& array, const TDatime& date ) = 0;
+  virtual Int_t PutArray( const char* system, const char* attr,
+			  const std::vector<Double_t>& array, const TDatime& date ) = 0;
+  
+  virtual Int_t PutArray( const char* system, const char* attr,
+			  const Int_t* array, Int_t size, const TDatime& date ) = 0;
+  virtual Int_t PutArray( const char* system, const char* attr,
+			  const Double_t* array, Int_t size, const TDatime& date ) = 0;
+  
+  virtual Int_t PutMatrix( const char* system, const char* name,
+			   const std::vector<std::vector<Double_t> >& rows,
+			   const TDatime& date ) = 0;
+  
+  virtual Int_t PutMatrix( const char* system, const char* name,
+			   const std::vector<std::vector<Int_t> >& rows,
+			   const TDatime& date ) = 0;
+
+  // specialized for elements with named rows, such as the TRANSPORT MATRIX
+  virtual Int_t PutMatrix( const char* system,
+			   const std::vector<std::string>& row_name,
+			   const std::vector<std::vector<Double_t> >& row_data,
+			   const TDatime& date ) = 0;
+
+  virtual  Int_t  StoreValues( const char* system, const TagDef* list,
+			       const TDatime& date ) = 0;
+  
+  // Set the comment associated with future DB writes
+  // weakly breaks the state-lessness of the class
+  virtual  void  SetDescription(const char* comment) { /* do nothing */ }
+
+  Int_t GetDetMap( const char* sysname, THaDetMap& detmap, const TDatime& date );
+
+  virtual  Int_t PutDetMap( const TDatime& date ) = 0;
+
+ protected:
+  virtual  Int_t LoadDetMap(const TDatime& date) = 0;
+  std::vector<THaDetConfig> fDetectorMap; // contains the detector configurations
+
+
+  typedef TagDef DBTagDef; // Create a private version for related class to use.
+  
+ public:
+  
+  ClassDef(THaDB,0)
+};
+
+class THaDetConfig {
+  
+ public:
+  THaDetConfig(std::string line);
+  bool IsGood() const;
+  virtual ~THaDetConfig();
+  
+ private:
+  std::string name;
+  int wir, roc, crate, slot;
+  int chan_lo, chan_hi;
+  std::string model;
+  
+ public:
+  friend class THaDB;
+  friend std::ostream& operator<<(std::ostream&, const THaDetConfig& );
+
+  ClassDef(THaDetConfig,0)
+};
+
+#endif  // HallA_THaDB
+
---
+++ src/THaDBFile.h
@@ -0,0 +1,172 @@
+#ifndef HallA_THaDBFile
+#define HallA_THaDBFile
+
+//////////////////////////////////////////////////////////////////////////
+//
+// THaDBFile
+//
+//  A simple class to read database from a key - value format txtfile
+//
+//  Date/time-based granularity is implemented, though clumsy in the
+//  file at the moment.
+// 
+//////////////////////////////////////////////////////////////////////////
+
+#include "TDatime.h"
+#include "THaDB.h"
+
+#include<string>
+#include<vector>
+#include<iostream>
+
+class THaDBFile : public THaDB {
+
+ public:
+  // constructor to connect to specific datafiles
+  THaDBFile(const char* calib="default.db",
+	     const char* detcfg="det.config",
+	     std::vector<THaDetConfig>* detmap=0);
+  
+  virtual ~THaDBFile();
+
+  Int_t GetValue( const char* system, const char* attr,
+		  Int_t& value, const TDatime &date );
+  Int_t GetValue( const char* system, const char* attr,
+		  Double_t& value, const TDatime &date );
+  Int_t GetValue( const char* system, const char* attr,
+		  std::string& value, const TDatime &date );
+
+  Int_t GetArray( const char* system, const char* attr,
+		  std::vector<Int_t>& array, const TDatime &date );
+  Int_t GetArray( const char* system, const char* attr,
+		  std::vector<Double_t>& array, const TDatime &date );
+
+  Int_t GetArray( const char* system, const char* attr,
+		  Int_t* array, Int_t size, const TDatime &date );
+  Int_t GetArray( const char* system, const char* attr,
+		  Double_t* array, Int_t size, const TDatime &date );
+
+  Int_t LoadValues ( const char* system, const TagDef* list,
+		     const TDatime& date );
+
+  Int_t GetMatrix( const char* system, const char* name,
+		   std::vector<std::vector<Int_t> >& rows,
+		   const TDatime& date );
+  Int_t GetMatrix( const char* system, const char* name,
+		   std::vector<std::vector<Double_t> >& rows,
+		   const TDatime& date );
+
+  // SPECIALIZED for the transport matrix with named rows
+  Int_t GetMatrix( const char* systemC,
+		   std::vector<std::string>& mtr_name,
+		   std::vector<std::vector<Double_t> >& mtr_rows,
+		   const TDatime& date );
+
+  Int_t PutValue( const char* system, const char* attr,
+		  const Int_t& value, const TDatime& date );
+  Int_t PutValue( const char* system, const char* attr,
+		  const Double_t& value, const TDatime& date );
+  Int_t PutValue( const char* system, const char* attr,
+		  const std::string& value, const TDatime& date );
+  
+  Int_t PutArray( const char* system, const char* attr,
+		  const std::vector<Int_t>& array, const TDatime& date );
+  Int_t PutArray( const char* system, const char* attr,
+		  const std::vector<Double_t>& array, const TDatime& date );
+  
+  Int_t PutArray( const char* system, const char* attr,
+		  const Int_t* array, Int_t size, const TDatime& date );
+  Int_t PutArray( const char* system, const char* attr,
+		  const Double_t* array, Int_t size, const TDatime& date );
+  
+  Int_t StoreValues( const char* system, const TagDef* list,
+		     const TDatime& date );
+  
+  Int_t PutMatrix( const char* system, const char* name,
+		   const std::vector<std::vector<Double_t> >& rows,
+		   const TDatime& date );
+  
+  Int_t PutMatrix( const char* system, const char* name,
+		   const std::vector<std::vector<Int_t> >& rows,
+		   const TDatime& date );
+  
+  // SPECIALIZED for the transport matrix with named rows
+  Int_t PutMatrix( const char* systemC,
+		   const std::vector<std::string>& mtr_name,
+		   const std::vector<std::vector<Double_t> >& mtr_rows,
+		   const TDatime& date );
+  
+  void  LoadDetCfgFile( const char* detcfg );
+  void  SetCalibFile( const char* calib );
+
+  void  SetDescription( const char* comment);
+
+  void  SetOutFile( const char* outfname );
+
+  Int_t PutDetMap( const TDatime& date );
+
+  int LoadDB();       // force reloading of database from file, what is in memory
+  
+  int FlushDB();      // Flush DB in memory to file
+
+  friend std::ostream& operator<<(std::ostream&, THaDBFile&);
+
+  void PrintDB() { cout << *this << endl; }
+  
+ protected:
+  Int_t LoadDetMap(const TDatime& date);
+  
+  std::string fInFileName;      // filename to read the calibration DB from
+  std::string fOutFileName;     // filename to write the DB to
+  std::string fDetFile;         // file to read the detector configuration from
+  std::string fDescription;     // tmp string to hold the description of current system
+
+ private:
+  bool find_constant(istream& from, int linebreak=0);
+  bool FindEntry( std::string& system, std::string& attr, std::istream& from,
+		  TDatime& date);
+
+  void WriteDate(std::ostream& to, const TDatime& date);
+  bool IsDate(std::istream& from, std::streampos &pos, TDatime& date );
+  
+  template<class T>
+    Int_t ReadValue( const char* systemC, const char* attrC,
+		     T& value, TDatime date);
+  template<class T>
+    Int_t ReadArray( const char* systemC, const char* attrC,
+		     vector<T>& array, TDatime date);
+  template<class T>
+    Int_t ReadArray( const char* systemC, const char* attrC,
+		     T* array, Int_t size, TDatime date );
+  template<class T>
+    Int_t ReadMatrix( const char* systemC, const char* attrC, 
+		      vector<vector<T> >& rows, TDatime date );
+  template<class T>
+    Int_t WriteValue( const char* systemC, const char* attrC,
+		      const T& value, TDatime date);
+  template<class T>
+    Int_t WriteArray( const char* system, const char* attr,
+		      const vector<T>& v, TDatime date);
+  template<class T>
+    Int_t WriteArray( const char* system, const char* attr,
+		      const T* array, Int_t size, 
+		      TDatime date);
+  template <class T>
+    Int_t WriteMatrix( const char* system, const char* attr,
+		       const vector<vector<T> >& matrix,
+		       TDatime date );
+
+  string db_contents; // everything in this database -- buffer for reading/writing
+  
+
+  int modified;
+  bool NextLine( std::istream& from );
+  bool CopyDB(std::istream& from, std::ostream& to, std::streampos pos=-1);
+  
+ public:
+  ClassDef(THaDBFile,0)
+
+};
+
+#endif  // HallA_THaDBFile
+
