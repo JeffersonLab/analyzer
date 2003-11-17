5155743855188b57443c89cf6001cd41f7d6fbc4
Ole Hansen <ole@jlab.org>
2003-11-17 17:12:59 +0000

    Version is 1.1

--- src/THaInterface.C
+++ src/THaInterface.C
@@ -107,7 +107,7 @@ void THaInterface::PrintLogo()
      mille = iyear;
    char* root_date = Form("%s %d %4d",months[imonth-1],iday,mille);
 
-   const char* halla_version = "1.1-cvs";
+   const char* halla_version = "1.1";
    //   const char* halla_date = Form("%d %s %4d",24,months[2-1],2003);
 
    Printf("  ************************************************");
@@ -117,7 +117,7 @@ void THaInterface::PrintLogo()
    Printf("  *                                              *");
    Printf("  *        Release %10s %18s *",halla_version,__DATE__);
    Printf("  *  Based on ROOT %8s %20s *",root_version,root_date);
-   Printf("  *             Development version              *");
+   //   Printf("  *             Development version              *");
    Printf("  *                                              *");
    Printf("  *            For information visit             *");
    Printf("  *        http://hallaweb.jlab.org/root/        *");
