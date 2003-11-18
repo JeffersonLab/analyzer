8a3f53380dfe8903f5cfee5d59b92aa15f2881a2
Bob Michaels <rom@jlab.org>
2003-11-18 14:52:30 +0000

    Test code to check epics decoding.  Similar style to the other *main* codes.  Compiled if STANDALONE=1 in Makefile, otherwise ignored

---
+++ hana_decode/epics_main.C
@@ -0,0 +1,76 @@
+//------------------------------------------------
+// epics_main.C   -- to find EPICS data and 
+//                   do something with them.
+// 
+#include <iostream>
+#include "THaCodaFile.h"
+#include "THaEvData.h"
+#include "TString.h"
+#include "evio.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+   if (argc < 2) {
+      cout << "You made a mistake... \n" << endl;
+      cout << "Usage:   epicsd filename" << endl;
+      cout << "  where 'filename' is the CODA file"<<endl;
+      cout << "\n... exiting." << endl;
+      exit(0);
+   }
+   TString filename = argv[1];
+   THaCodaFile datafile;
+   if (datafile.codaOpen(filename) != S_SUCCESS) {
+        cout << "ERROR:  Cannot open CODA data" << endl;
+        cout << "Perhaps you mistyped it" << endl;
+        cout << "... exiting." << endl;
+        exit(0);
+   }
+      
+   THaEvData evdata;
+
+// Loop over a finite number of events
+
+   int NUMEVT=1000000;
+   for (int i=0; i<NUMEVT; i++) {
+
+     int status = datafile.codaRead();  
+	                                            
+     if ( status != S_SUCCESS ) {
+        if ( status == EOF) {
+           cout << "This is end of file !" << endl;
+           cout << "... exiting " << endl;
+           exit(1);
+        } else {
+  	   cout << hex << "ERROR: codaRread status = " << status << endl;
+           exit(0);
+        }
+     }
+
+     evdata.LoadEvent( datafile.getEvBuffer() );   
+
+     if(evdata.IsEpicsEvent()) {
+
+       cout << "Some epics data --> "<<endl;
+       cout << "hac_bcm_average "<<
+	 evdata.GetEpicsData("hac_bcm_average")<<endl;
+       cout << "IPM1H04A.XPOS  "<<
+         evdata.GetEpicsData("IPM1H04A.XPOS")<<endl;
+       cout << "IPM1H04A.YPOS  "<<
+         evdata.GetEpicsData("IPM1H04A.YPOS")<<endl;
+
+     }
+
+   }  //  end of event loop
+
+
+}
+
+
+
+
+
+
+
