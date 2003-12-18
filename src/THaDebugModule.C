da5ba472dea8a2e2107c5f051627d2bb96569cac
Ole Hansen <ole@jlab.org>
2003-12-18 20:58:14 +0000

    Also allow inspection of cuts.

--- src/THaDebugModule.C
+++ src/THaDebugModule.C
@@ -10,13 +10,13 @@
 //////////////////////////////////////////////////////////////////////////
 
 #include "THaDebugModule.h"
-#include "THaVar.h"
 #include "THaVarList.h"
 #include "THaCutList.h"
 #include "THaCut.h"
 #include "THaGlobals.h"
 #include "THaEvData.h"
 #include "TRegexp.h"
+#include "TClass.h"
 
 #include <cstring>
 #include <cctype>
@@ -24,17 +24,16 @@
 
 using namespace std;
 
-typedef vector<const THaVar*>::const_iterator VIter_t;
+typedef vector<const TObject*>::const_iterator VIter_t;
 
 //_____________________________________________________________________________
 THaDebugModule::THaDebugModule( const char* var_list, const char* test ) :
-  THaPhysicsModule("DebugModule",var_list), 
+  THaPhysicsModule("DebugModule",var_list),
   fVarString(var_list), fFlags(kStop), fCount(0), fTestExpr(test), fTest(0)
 {
   // Normal constructor.
 
-  // Flag indicating that test pointer needs to be assigned
-  fIsSetup = fTestExpr.IsNull();
+  fIsSetup = false;
 }
 
 //_____________________________________________________________________________
@@ -47,19 +46,27 @@ THaDebugModule::~THaDebugModule()
 //_____________________________________________________________________________
 THaAnalysisObject::EStatus THaDebugModule::Init( const TDatime& run_time )
 {
-  // Initialize the module.
+  // Initialize the module. This just clears the list of variables.
+  // The actual initialization takes place in ParseList()
+
+  fVars.clear();
 
   if( THaPhysicsModule::Init( run_time ))
     return fStatus;
 
-  fVars.clear();
+  return fStatus = kOK;
+}
+
+
+//_____________________________________________________________________________
+Int_t THaDebugModule::THaDebugModule::ParseList()
+{
+  // Parse the list of variable/cut names and store pointers
+  // to the found objects. Called from Process() the first time
+  // this module is used.
 
   Int_t nopt = fVarString.GetNOptions();
   if( nopt > 0 ) {
-    if( !gHaVars )
-      return fStatus = kInitError;
-    TIter next( gHaVars );
-    TObject* obj;
     for( Int_t i=0; i<nopt; i++ ) {
       const char* opt = fVarString.GetOption(i);
       if( !strcmp(opt,"NOSTOP") )
@@ -68,22 +75,39 @@ THaAnalysisObject::EStatus THaDebugModule::Init( const TDatime& run_time )
 	// Regexp matching
 	bool found = false;
 	TRegexp re( opt, kTRUE);
-	next.Reset();
-	while( (obj = next()) ) {
-	  TString s = obj->GetName();
-	  if( s.Index(re) != kNPOS ) {
-	    found = true;
-	    fVars.push_back( static_cast<const THaVar*>(obj) );
+	TObject* obj;
+	// We can inspect analysis variables and cuts/tests
+	if( gHaVars ) {
+	  TIter next( gHaVars );
+	  while( (obj = next()) ) {
+	    TString s = obj->GetName();
+	    if( s.Index(re) != kNPOS ) {
+	      found = true;
+	      fVars.push_back(obj);
+	    }
+	  }
+	}
+	if( gHaCuts ) {
+	  const TList* lst = gHaCuts->GetCutList();
+	  if( lst ) {
+	    TIter next( lst );
+	    while( (obj = next()) ) {
+	      TString s = obj->GetName();
+	      if( s.Index(re) != kNPOS ) {
+		found = true;
+		fVars.push_back(obj);
+	      }
+	    }
 	  }
 	}
 	if( !found ) {
-	  Warning( Here("Init()"), 
-		   "Global variable %s not found. Skipped.", opt );
+	  Warning( Here("ParseList"), 
+		   "Global variable or cut %s not found. Skipped.", opt );
 	}
       }
     }
   }
-  return fStatus = kOK;
+  return 0;
 }
 
 //_____________________________________________________________________________
@@ -127,6 +151,7 @@ Int_t THaDebugModule::Process( const THaEvData& evdata )
   // called before the analyzer's tests are loaded, and we want to be
   // able to use existing tests.
   if( !fIsSetup ) {
+    ParseList();
     fTest = new THaCut( fName+"_Test", fTestExpr, fName+"_Block" );
     // Expression error?
     if( !fTest || fTest->IsZombie()) {
@@ -142,7 +167,10 @@ Int_t THaDebugModule::Process( const THaEvData& evdata )
     PrintEvNum( evdata );
     VIter_t it = fVars.begin();
     for( ; it != fVars.end(); it++ ) {
-      (*it)->Print();
+      const char* opt = "";
+      if( (*it)->IsA()->InheritsFrom("THaVar") ) 
+	opt = "FULL";
+      (*it)->Print(opt);
     }
   }
 
