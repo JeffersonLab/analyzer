7404498c4c4c087d063c3ec91e74bef1bda8a7d9
Ole Hansen <ole@jlab.org>
2004-04-12 20:30:23 +0000

    Explicitly link some common STL template instances so they can be used in CINT macros.

--- hana_scaler/haScal_LinkDef.h
+++ hana_scaler/haScal_LinkDef.h
@@ -4,11 +4,16 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class THaScaler;
-#pragma link C++ class THaScalerBank;
-#pragma link C++ class THaNormScaler;
-#pragma link C++ class THaScalerDB;
-// #pragma link C++ class THaScalerGui;
+#pragma link C++ class THaScaler+;
+#pragma link C++ class THaScalerBank+;
+#pragma link C++ class THaNormScaler+;
+#pragma link C++ class THaScalerDB+;
+// #pragma link C++ class THaScalerGui+;
+
+#ifdef __MAKECINT__
+#pragma link C++ class std::map<std::string,int>;
+#pragma link C++ class std::map<int,std::string>;
+#endif
 
 #endif
 
