2cd7d684eaa508ab2f835d6b02d81a8da2ded124
Ole Hansen <ole@jlab.org>
2004-04-12 20:31:00 +0000

    Link classes used ROOT 3.x streamer mechanism.

--- hana_decode/haDecode_LinkDef.h
+++ hana_decode/haDecode_LinkDef.h
@@ -4,18 +4,18 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class THaCodaData;
-#pragma link C++ class THaCodaFile;
-#pragma link C++ class THaCrateMap;
-#pragma link C++ class THaEpics;
-#pragma link C++ class THaEvData;
-#pragma link C++ class THaFastBusWord;
-#pragma link C++ class THaHelicity;
-#pragma link C++ class THaSlotData;
-#pragma link C++ class THaUsrstrutils;
+#pragma link C++ class THaCodaData+;
+#pragma link C++ class THaCodaFile+;
+#pragma link C++ class THaCrateMap+;
+#pragma link C++ class THaEpics+;
+#pragma link C++ class THaEvData+;
+#pragma link C++ class THaFastBusWord+;
+#pragma link C++ class THaHelicity+;
+#pragma link C++ class THaSlotData+;
+#pragma link C++ class THaUsrstrutils+;
 
 #ifdef ONLINE_ET
-#pragma link C++ class THaEtClient;
+#pragma link C++ class THaEtClient+;
 #endif
 
 #endif
