84e0e2c1b7942fb40fa3115e084b199e964046d4
Ole Hansen <ole@jlab.org>
2004-04-27 20:41:19 +0000

    Initial version of beam information ABI for physics modules, analogous to THaTrackInfo.

---
+++ src/THaBeamInfo.C
@@ -0,0 +1,78 @@
+//*-- Author :    Ole Hansen   27-Apr-04
+
+//////////////////////////////////////////////////////////////////////////
+//
+// THaBeamInfo
+//
+// Utility class/structure for holding variable beam information.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "THaBeamInfo.h"
+#include "THaRunBase.h"
+#include "THaRunParameters.h"
+#include "THaGlobals.h"
+#include "TMath.h"
+
+using namespace std;
+
+const Double_t THaBeamInfo::kBig = 1e38;
+
+//_____________________________________________________________________________
+Double_t THaBeamInfo::GetE() const
+{
+  // Return beam energy. This is the energy corresponding to the
+  // current beam momentum and mass, not necessarily the same as
+  // the nominal beam energy in the run database.
+
+  if( !gHaRun )
+    return kBig;
+  THaRunParameters* rp = gHaRun->GetParameters();
+  if( !rp )
+    return kBig;
+  Double_t m = rp->GetBeamM();
+  Double_t p = GetP();
+  return TMath::Sqrt( p*p + m*m );
+}
+
+//_____________________________________________________________________________
+Double_t THaBeamInfo::GetM() const
+{
+  // Return mass of beam particles
+
+  if( !gHaRun )
+    return kBig;
+  THaRunParameters* rp = gHaRun->GetParameters();
+  if( !rp )
+    return kBig;
+  return rp->GetBeamM();
+}
+
+//_____________________________________________________________________________
+Double_t THaBeamInfo::GetQ() const
+{
+  // Return charge of beam particles (electrons: -1)
+
+  if( !gHaRun )
+    return kBig;
+  THaRunParameters* rp = gHaRun->GetParameters();
+  if( !rp )
+    return kBig;
+  return rp->GetBeamQ();
+}
+
+//_____________________________________________________________________________
+Double_t THaBeamInfo::GetdE() const
+{
+  // Return beam energy uncertainty
+
+  if( !gHaRun )
+    return kBig;
+  THaRunParameters* rp = gHaRun->GetParameters();
+  if( !rp )
+    return kBig;
+  return rp->GetBeamdE();
+}
+
+//_____________________________________________________________________________
+ClassImp(THaBeamInfo)
---
+++ src/THaBeamInfo.h
@@ -0,0 +1,91 @@
+#ifndef ROOT_THaBeamInfo
+#define ROOT_THaBeamInfo
+
+//////////////////////////////////////////////////////////////////////////
+//
+// THaBeamInfo
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "TVector3.h"
+class THaBeam;
+
+class THaBeamInfo {
+public:
+  THaBeamInfo() : fPosition(kBig,kBig,kBig), fPvect(kBig,kBig,kBig), fPol(0.0),
+		  fOK(0), fBeam(0) {}
+  THaBeamInfo( const TVector3& pvect, const TVector3& position, Double_t pol = 0.0 ) 
+    : fPosition(position), fPvect(pvect), fPol(pol), fOK(1), fBeam(0) {}
+  THaBeamInfo( Double_t p, const TVector3& vect, const TVector3& position, 
+	       Double_t pol = 0.0 ) 
+    : fPosition(position), fPvect(vect), fPol(pol), fOK(1), fBeam(0) { SetP(p); }
+
+  THaBeamInfo( const THaBeamInfo& t ) :
+    fPosition(t.fPosition), fPvect(t.fPvect), fPol(t.fPol), fOK(t.fOK),
+    fBeam(t.fBeam) {}
+  THaBeamInfo& operator=( const THaBeamInfo& rhs ) {
+    if( this != &rhs ) {
+      fPosition = rhs.fPosition;
+      fPvect    = rhs.fPvect;
+      fPol      = rhs.fPol;
+      fOK       = rhs.fOK;
+      fBeam     = rhs.fBeam;
+    }
+    return *this;
+  }
+  virtual ~THaBeamInfo() {}
+
+  void      Clear( Option_t* opt="" ) { 
+    fPvect.SetXYZ(kBig,kBig,kBig); fPosition.SetXYZ(kBig,kBig,kBig);
+    fPol = 0.0; fOK = 0; 
+  }
+
+  Bool_t    IsOK()     const { return fOK; }
+  Double_t  GetPx()    const { return fPvect.X(); }
+  Double_t  GetPy()    const { return fPvect.Y(); }
+  Double_t  GetPz()    const { return fPvect.Z(); }
+  Double_t  GetP()     const { return fPvect.Mag(); }
+  Double_t  GetX()     const { return fPosition.X(); }
+  Double_t  GetY()     const { return fPosition.Y(); }
+  Double_t  GetZ()     const { return fPosition.Z(); }
+  Double_t  GetTheta() const { return (GetPz() != 0.0) ? GetPx()/GetPz():kBig;}
+  Double_t  GetPhi()   const { return (GetPz() != 0.0) ? GetPy()/GetPz():kBig;}
+  Double_t  GetPol()   const { return fPol; }
+  Double_t  GetE()     const;
+  Double_t  GetM()     const;
+  Double_t  GetQ()     const;
+  Double_t  GetdE()    const;
+  const TVector3& GetPvect()    const { return fPvect; }
+  const TVector3& GetPosition() const { return fPosition; }
+
+  THaBeam*  GetBeam() const  { return fBeam; }
+
+  void SetP( Double_t p )     { fPvect.SetMag(p); }
+  void SetPol( Double_t pol ) { fPol = pol; }
+  void Set( const TVector3& pvect, const TVector3& position, 
+	    Double_t pol = 0.0 ) {
+    fPvect = pvect; fPosition = position; SetPol(pol); fOK = 1;
+  }
+  void Set( Double_t p, const TVector3& vect, const TVector3& position,
+	    Double_t pol = 0.0 ) { 
+    fPvect = vect; SetP(p); fPosition = position; SetPol(pol); fOK = 1;
+  }
+  void SetBeam(THaBeam* obj)  { fBeam = obj; }
+
+protected:
+  TVector3  fPosition;  // Reference position in lab frame (m)
+  TVector3  fPvect;     // Momentum vector in lab (GeV/c)
+  Double_t  fPol;       // Beam polarization
+  Int_t     fOK;        // Data ok (0:no 1:yes)
+
+  THaBeam*  fBeam;      //! Beam apparatus for this beam information
+
+private:
+  static const Double_t kBig;
+
+  ClassDef(THaBeamInfo,1)  // Beam information for physics modules
+};
+
+
+#endif
+
