5b1444620d3de2a5c910354cc84a3b5df5dd8551
Ole Hansen <ole@jlab.org>
2004-05-11 19:16:41 +0000

    First version of a beam energy loss correction module.  A beam eloss module corrects for energy loss _before_ the interaction (i.e. for incident particles), while a track eloss module corrects eloss _after_ the reaction (i.e. for outgoing particles).

---
+++ src/THaBeamEloss.C
@@ -0,0 +1,145 @@
+//*-- Author :    Ole Hansen   11-May-04
+
+//////////////////////////////////////////////////////////////////////////
+//
+// THaBeamEloss
+//
+// Correct energy loss of the beam by a constant. This is basically trivial,
+// but useful to have to provide input for kinematics modules. 
+// More advanced corrections can be implemented by inheritance from
+// this module.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "THaBeamEloss.h"
+#include "TMath.h"
+#include "TVector3.h"
+#include "VarDef.h"
+#include <iostream>
+
+using namespace std;
+
+//_____________________________________________________________________________
+THaBeamEloss::THaBeamEloss( const char* name, const char* description,
+			    const char* input_beam ) :
+  THaElossCorrection(name,description,input_beam), fBeamModule(NULL)
+{
+  // Normal constructor.
+
+  Clear();
+}
+
+//_____________________________________________________________________________
+THaBeamEloss::~THaBeamEloss()
+{
+  // Destructor
+
+  DefineVariables( kDelete );
+}
+
+//_____________________________________________________________________________
+void THaBeamEloss::CalcEloss( THaBeamInfo* beamifo )
+{
+  // Compute the mean energy loss for the beam given in beamifo.
+  //
+  // Currently, we use a very simple algorithm that computes
+  // the energy loss based on a fixed material thickness.
+  // Only the beta dependence of the energy loss is used,
+  // but there are no corrections for angle, pathlength etc.
+  //
+  // May be overridden by derived classes as necessary.
+
+  Double_t p0 = beamifo->GetP();
+  Double_t beta = p0 / TMath::Sqrt(p0*p0 + fM*fM);
+  if( fElectronMode ) {
+    fEloss = ElossElectron( beta, fZmed, fAmed, 
+			    fDensity, fPathlength );
+  } else {
+    fEloss = ElossHadron( fZ, beta, fZmed, fAmed, 
+			  fDensity, fPathlength );
+  }
+}
+
+//_____________________________________________________________________________
+void THaBeamEloss::Clear( Option_t* opt )
+{
+  // Clear all event-by-event variables.
+  
+  THaElossCorrection::Clear(opt);
+  BeamIfoClear();
+}
+
+//_____________________________________________________________________________
+THaAnalysisObject::EStatus THaBeamEloss::Init( const TDatime& run_time )
+{
+  // Initialize the module.
+  // Locate the input beam module named in fInputName and save pointer to it. 
+  // Extract mass and charge of the beam particles from the input.
+
+  // Find the input beam module
+  fBeamModule = dynamic_cast<THaBeamModule*>
+    ( FindModule( fInputName.Data(), "THaBeamModule"));
+
+  // Extract the beam particle parameters from the input
+  THaBeamInfo* beamifo = fBeamModule->GetBeamInfo();
+  if( !beamifo->IsOK() )
+    return fStatus = kInitError;
+
+  // overrides anything set by SetMass()
+  SetMass( beamifo->GetM() );
+  fZ = TMath::Abs(beamifo->GetQ());
+
+  // Standard initialization. Calls this object's DefineVariables().
+  // Reads material parameters from the run database
+  THaElossCorrection::Init( run_time );
+
+  return fStatus;
+}
+
+//_____________________________________________________________________________
+Int_t THaBeamEloss::DefineVariables( EMode mode )
+{
+  // Define/delete global variables.
+
+  if( mode == kDefine && fIsSetup ) return kOK;
+  THaElossCorrection::DefineVariables( mode );
+
+  // Define the variables for the beam info subobject
+  return DefineVarsFromList( GetRVarDef(), mode );
+}
+
+//_____________________________________________________________________________
+Int_t THaBeamEloss::Process( const THaEvData& evdata )
+{
+  // Calculate corrections and adjust the track parameters.
+
+  if( !IsOK() ) return -1;
+
+  THaBeamInfo* beamifo = fBeamModule->GetBeamInfo();
+  if( !beamifo->IsOK() ) return 2;
+
+  // Copy the input track info
+  fBeamIfo = *beamifo;
+  
+  // Compute the correction. Remember that we want to compute the
+  // beam energy at the reaction vertex, so we need to subtract the
+  // energy loss from the input energy.
+  Double_t p_in = fBeamIfo.GetP(); 
+  if( p_in <= 0.0 ) return 4; //oops
+  Double_t E_in = TMath::Sqrt(p_in*p_in + fM*fM);
+  if( !fTestMode ) {
+    //update fEloss
+    CalcEloss(beamifo);
+  }
+  Double_t p_out = TMath::Sqrt(p_in*p_in + fEloss*fEloss - 2.0*E_in*fEloss);
+  
+  // Apply the corrected momentum to our beam info
+  fBeamIfo.SetP(p_out);
+
+  fDataValid = true;
+  return 0;
+}
+
+//_____________________________________________________________________________
+ClassImp(THaBeamEloss)
+
---
+++ src/THaBeamEloss.h
@@ -0,0 +1,39 @@
+#ifndef ROOT_THaBeamEloss
+#define ROOT_THaBeamEloss
+
+//////////////////////////////////////////////////////////////////////////
+//
+// THaBeamEloss
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "THaElossCorrection.h"
+#include "THaBeamModule.h"
+
+class THaBeamEloss : public THaElossCorrection, public THaBeamModule {
+  
+public:
+  THaBeamEloss( const char* name, const char* description,
+		const char* input_beam = "" );
+  virtual ~THaBeamEloss();
+  
+  virtual void      Clear( Option_t* opt="" );
+
+  virtual EStatus   Init( const TDatime& run_time );
+  virtual Int_t     Process( const THaEvData& );
+
+
+protected:
+
+  THaBeamModule*    fBeamModule; // Pointer to beam module
+
+  // Function for updating fEloss based on input beamifo.
+  virtual void  CalcEloss( THaBeamInfo* beamifo );
+
+  // Setup functions
+  virtual Int_t DefineVariables( EMode mode = kDefine );
+
+  ClassDef(THaBeamEloss,0)   //Beam energy loss correction module
+};
+
+#endif
