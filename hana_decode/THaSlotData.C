4541953512fa8e522a0e7ac4469040e2636b7ba2
Bodo Reitz <reitz@jlab.org>
2004-07-19 22:05:53 +0000

    can handle modules where the data from different channels comes randomly ( = not sorted by channels ) - needed for CAEN V767 TDCs it is still assumed that within one channel data is sorted by time or ADCvalue and of course this one is slower and needs more memory than the old one

--- hana_decode/THaSlotData.C
+++ hana_decode/THaSlotData.C
@@ -24,29 +24,36 @@
 using namespace std;
 
 static const bool VERBOSE = true;
-const int THaSlotData::DEFNCHAN = 100;  // Default number of channels
+const int THaSlotData::DEFNCHAN = 128;  // Default number of channels
 const int THaSlotData::DEFNDATA = 1024; // Default number of data words
+const int THaSlotData::DEFNHITCHAN = 1; // Default number of hits per channel
 
 THaSlotData::THaSlotData() : 
-  crate(-1), slot(-1), numraw(0), numchanhit(0), numHits(0),
-  chanlist(0), chanindex(0), rawData(0), data(0), didini(false),
-  maxc(0), maxd(0), allocd(0) {}
+  crate(-1), slot(-1), numhitperchan(0), numraw(0), numchanhit(0), firstfreedataidx(0), 
+  numholesdataidx(0), numHits(0), chanlist(0), idxlist (0), chanindex(0), dataindex(0), 
+  numMaxHits(0), rawData(0), data(0), didini(false),
+  maxc(0), maxd(0), allocd(0), alloci(0) {}
 
 THaSlotData::THaSlotData(int cra, int slo) :
-  crate(cra), slot(slo), numraw(0), numchanhit(0), numHits(0),
-  chanlist(0), chanindex(0), rawData(0), data(0), didini(false),
-  maxc(0), maxd(0), allocd(0) {}
+  crate(cra), slot(slo), numhitperchan(0), numraw(0), numchanhit(0), firstfreedataidx(0), 
+  numholesdataidx(0), numHits(0), chanlist(0), idxlist (0), chanindex(0), dataindex(0), 
+  numMaxHits(0), rawData(0), data(0), didini(false),
+  maxc(0), maxd(0), allocd(0), alloci(0) {}
+
 
 THaSlotData::~THaSlotData() {
   if( !didini ) return;
   delete [] numHits;
   delete [] chanlist;
+  delete [] idxlist;
   delete [] chanindex;
+  delete [] dataindex;
+  delete [] numMaxHits;
   delete [] rawData;
   delete [] data;
 }
 
-void THaSlotData::define(int cra, int slo, UShort_t nchan, UShort_t ndata ) {
+void THaSlotData::define(int cra, int slo, UShort_t nchan, UShort_t ndata, UShort_t nhitperchan ) {
   // Must call define once if you are really going to use this slot.
   // Otherwise its an empty slot which does not use much memory.
   crate = cra;
@@ -54,26 +61,35 @@ void THaSlotData::define(int cra, int slo, UShort_t nchan, UShort_t ndata ) {
   didini = true;
   maxc = nchan;
   maxd = ndata;
+  numhitperchan=nhitperchan;
   // Initial allocation of data arrays
   allocd = nchan;
+  alloci = nchan;
   // Delete arrays if defined so we can call define() more than once!
-  delete [] numHits; delete [] chanlist; delete [] chanindex;
-  delete [] rawData; delete [] data;
+  delete [] numHits;
+  delete [] chanlist;
+  delete [] idxlist;
+  delete [] chanindex;
+  delete [] dataindex;
+  delete [] numMaxHits;
+  delete [] rawData;
+  delete [] data;
   numHits   = new UChar_t[maxc];
   chanlist  = new UShort_t[maxc];
+  idxlist  = new UShort_t[maxc];
   chanindex = new UShort_t[maxc];
   rawData   = new int[allocd];
   data      = new int[allocd];
-  numchanhit = numraw = 0;
+  dataindex = new UShort_t[alloci];
+  numMaxHits = new UChar_t[maxc];
+  numchanhit = numraw = firstfreedataidx = numholesdataidx= 0;
   memset(numHits,0,maxc*sizeof(UChar_t));
 }
 
 int THaSlotData::loadData(const char* type, int chan, int dat, int raw) {
 // loadData loads the data into storage arrays.
-// Note, this algorithm relies on channels being read out
-// in a sequential, not random, order.
 
-  static int very_verb=0;
+  static int very_verb=1;
 
   if( !didini ) {
     if (very_verb) {  // this might be your problem.
@@ -99,11 +115,38 @@ int THaSlotData::loadData(const char* type, int chan, int dat, int raw) {
     return SD_ERR;
   }
   if( device.IsNull() ) device = type;
-  if( numchanhit == 0 || chan != chanlist[numchanhit-1] ) {
-    //New channel
-    chanindex[chan] = numraw;
+
+  if (( numchanhit == 0 )||(numHits[chan]==0)) {
+    compressdataindex(numhitperchan);
+    dataindex[firstfreedataidx]=numraw;
+    idxlist[chan]=firstfreedataidx;
+    numMaxHits[chan]=numhitperchan;
+    firstfreedataidx=firstfreedataidx+numhitperchan;
+    chanindex[chan]=numchanhit;
     chanlist[numchanhit++]=chan;
+  } else {
+    if (numHits[chan]<numMaxHits[chan]) {
+      dataindex[idxlist[chan]+numHits[chan]]=numraw;
+    } else {
+      if (idxlist[chan]+numMaxHits[chan]==firstfreedataidx) {
+	compressdataindex(numhitperchan);
+	dataindex[idxlist[chan]+numHits[chan]]=numraw;
+	numMaxHits[chan]=numMaxHits[chan]+numhitperchan;
+	firstfreedataidx=firstfreedataidx+numhitperchan;
+      } else {
+	compressdataindex(numMaxHits[chan]+numhitperchan);
+	numholesdataidx=numholesdataidx+numMaxHits[chan];
+	for (Int_t i=0; i<numHits[chan]; i++  ) {
+	  dataindex[firstfreedataidx+i]=dataindex[idxlist[chan]+i];
+	}
+	dataindex[firstfreedataidx+numHits[chan]]=numraw;
+	idxlist[chan]=firstfreedataidx;
+	numMaxHits[chan]=numMaxHits[chan]+numhitperchan;
+	firstfreedataidx=firstfreedataidx+numMaxHits[chan];
+      }
+    }
   }
+
   // Grow data arrays if really necessary (rare)
   if( numraw >= allocd ) {
     UShort_t old_allocd = allocd;
@@ -128,6 +171,7 @@ int THaSlotData::loadData(const char* type, int chan, int dat, int raw) {
   numHits[chan]++;
   return SD_OK;
 }
+
 
 void THaSlotData::print() const {
   cout << "\n THaSlotData contents : " << endl;
