8d4c9282820520690de5ff5973d85eb4e396a4d5
Rob Feuerbach <feuerbac@jlab.org>
2006-03-18 01:37:17 +0000

    Intial version of libGenDecData library making environment. Not where I want it to be, but the Gen* objects are in the src/ directory.

---
+++ src/GenDecData_LinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class GenDecData+;
+#pragma link C++ class GenBeam+;
+#pragma link C++ class GenBPM+;
+#pragma link C++ class GenHelicity+;
+#pragma link C++ class GenRaster+;
+
+#endif
