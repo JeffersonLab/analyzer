937e4e5bd26559438759379f098fbba6bed7abea
rwmichaels <rom@jlab.org>
2013-12-04 11:55:16 -0500

    main (first) code for testing Toy decoder

---
+++ oodecoder/tstoy_main.C
@@ -0,0 +1,113 @@
+// Test of Toy decoder model for the OO decoder upgrade
+// Humble beginnigns
+// R. Michaels, Dec 2013
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include "THaCodaFile.h"
+#include "ToyCodaDecoder.h"
+#include "THaEvData.h"
+#include "evio.h"
+#include "THaSlotData.h"
+#include "TString.h"
+
+using namespace std;
+
+void dump(int *buffer);
+void process(THaEvData *evdata);
+
+int main(int argc, char* argv[])
+{
+
+   TString filename("snippet.dat");
+
+   THaCodaFile datafile(filename);
+   THaEvData *evdata = new ToyCodaDecoder();
+
+    // Loop over events
+      int NUMEVT=100;
+      for (int iev=0; iev<NUMEVT; iev++) {
+   	 int status = datafile.codaRead();  
+         if (status != S_SUCCESS) {
+	   if ( status == EOF) {
+             cout << "Normal end of file.  Bye bye." << endl;
+	   } else {
+  	     cout << hex << "ERROR: codaRread status = " << status << endl;
+ 	   }
+           exit(1);
+	 } else {
+
+            int *data = datafile.getEvBuffer();
+  	    dump(data);
+
+            evdata->LoadEvent( data );   
+
+            process (evdata);
+
+
+	 }
+      }
+
+ 
+}
+     
+
+void dump( int* data) {
+    // Crude event dump
+            int evnum = data[4];
+            int len = data[0] + 1;
+            int evtype = data[1]>>16;
+            cout << "\n\n Event number " << dec << evnum << endl;
+            cout << " length " << len << " type " << evtype << endl;
+            int ipt = 0;
+	    for (int j=0; j<(len/5); j++) {
+	      cout << dec << "\n evbuffer[" << ipt << "] = ";
+              for (int k=j; k<j+5; k++) {
+		cout << hex << data[ipt++] << " ";
+	      }
+              cout << endl;
+	    }
+            if (ipt < len) {
+	      cout << dec << "\n evbuffer[" << ipt << "] = ";
+              for (int k=ipt; k<len; k++) {
+		cout << hex << data[ipt++] << " ";
+              }
+              cout << endl;
+            }
+}
+
+void process (THaEvData *evdata) {
+
+     cout << "\n\nHello.  Now we process evdata : "<<endl;
+
+     cout << "\nEvent type   " << dec << evdata->GetEvType() << endl;
+     cout << "Event number " << evdata->GetEvNum()  << endl;
+     cout << "Event length " << evdata->GetEvLength() << endl;
+     if (evdata->IsPhysicsTrigger() ) { // triggers 1-14
+        cout << "Physics trigger " << endl;
+     }
+     if(evdata->IsScalerEvent()) cout << "Scaler `event' " << endl;
+
+// Now we want data from a particular crate and slot.
+// E.g. crates are 1,2,3,13,14,15 (roc numbers), Slots are 1,2,3... 
+// This is like what one might do in a detector decode() routine.
+
+      int crate = 1;    // for example
+      int slot = 24;
+
+//  Here are raw 32-bit CODA words for this crate and slot
+      cout << "Raw Data Dump for crate "<<dec<<crate<<" slot "<<slot<<endl; 
+      int hit;
+      for(hit=0; hit<evdata->GetNumRaw(crate,slot); hit++) {
+        cout<<dec<<"raw["<<hit<<"] =   ";
+        cout<<hex<<evdata->GetRawData(crate,slot,hit)<<endl;  
+      }
+// You can alternatively let evdata print out the contents of a crate and slot:
+      evdata->PrintSlotData(crate,slot);
+
+}
+
+
+
+
