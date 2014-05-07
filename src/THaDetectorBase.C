6a148c3b5aecc4ff48a102aea2e6852aa98432dc
Ole Hansen <ole@jlab.org>
2014-05-07 14:23:36 -0400

    Add THaDetectorBase::IsInActiveArea. Better error checking in ReadGeometry.
    This allows quick checking of track projections etc. against the
    detector geometry.

--- src/THaDetectorBase.C
+++ src/THaDetectorBase.C
@@ -6,8 +6,8 @@
 //
 // Abstract base class for a detector or subdetector.
 //
-// Derived classes must define all internal variables, a constructor 
-// that registers any internal variables of interest in the global 
+// Derived classes must define all internal variables, a constructor
+// that registers any internal variables of interest in the global
 // physics variable list, and a Decode() method that fills the variables
 // based on the information in the THaEvData structure.
 //
@@ -15,12 +15,13 @@
 
 #include "THaDetectorBase.h"
 #include "THaDetMap.h"
+#include "TMath.h"
 #include "VarType.h"
 
 using std::vector;
 
 //_____________________________________________________________________________
-THaDetectorBase::THaDetectorBase( const char* name, 
+THaDetectorBase::THaDetectorBase( const char* name,
 				  const char* description ) :
   THaAnalysisObject(name,description), fNelem(0)
 {
@@ -46,7 +47,7 @@ THaDetectorBase::~THaDetectorBase()
 Int_t THaDetectorBase::FillDetMap( const vector<Int_t>& values, UInt_t flags,
 				   const char* here )
 {
-  // Utility function to fill this detector's detector map. 
+  // Utility function to fill this detector's detector map.
   // See THaDetMap::Fill for documentation.
 
   Int_t ret = fDetMap->Fill( values, flags );
@@ -63,6 +64,18 @@ Int_t THaDetectorBase::FillDetMap( const vector<Int_t>& values, UInt_t flags,
 }
 
 //_____________________________________________________________________________
+Bool_t THaDetectorBase::IsInActiveArea( Double_t x, Double_t y ) const
+{
+  // Check if given (x,y) coordinates are inside this detector's active area
+  // (defined by fOrigin/fSize)
+
+  return ( x >= fOrigin.X()-fSize[0] &&
+	   x <= fOrigin.X()+fSize[0] &&
+	   y >= fOrigin.Y()-fSize[1] &&
+	   y <= fOrigin.Y()+fSize[1] );
+}
+
+//_____________________________________________________________________________
 void THaDetectorBase::PrintDetMap( Option_t* opt ) const
 {
   // Print out detector map.
@@ -72,10 +85,12 @@ void THaDetectorBase::PrintDetMap( Option_t* opt ) const
 
 //_____________________________________________________________________________
 Int_t THaDetectorBase::ReadGeometry( FILE* file, const TDatime& date,
-				     Bool_t required ) 
+				     Bool_t required )
 {
-  // Read this detector's basic geometry information from the database. 
+  // Read this detector's basic geometry information from the database.
   // Derived classes may override to read more advanced data.
+
+  const char* const here = __FUNCTION__;
 
   vector<double> position, size;
   Bool_t optional = !required;
@@ -92,21 +107,31 @@ Int_t THaDetectorBase::ReadGeometry( FILE* file, const TDatime& date,
 
   if( !position.empty() ) {
     if( position.size() != 3 ) {
-      Error( Here("ReadGeometry"), "Incorrect number of values for "
-	     "detector position" );
+      Error( Here(here), "Incorrect number of values = %u for "
+	     "detector position. Must be exactly 3. Fix database.",
+	     static_cast<unsigned int>(position.size()) );
       return 1;
     }
     fOrigin.SetXYZ( position[0], position[1], position[2] );
   }
   if( !size.empty() ) {
     if( size.size() != 3 ) {
-      Error( Here("ReadGeometry"), "Incorrect number of values for "
-	     "detector size" );
+      Error( Here(here), "Incorrect number of values = %u for "
+	     "detector size. Must be exactly 3. Fix database.",
+	     static_cast<unsigned int>(size.size()) );
       return 2;
     }
-    fSize[0] = size[0]/2.0;
-    fSize[1] = size[1]/2.0;
-    fSize[2] = size[2];
+    if( size[0] == 0 || size[1] == 0 || size[2] == 0 ) {
+      Error( Here(here), "Illegal zero detector dimension. Fix database." );
+      return 3;
+    }
+    if( size[0] < 0 || size[1] < 0 || size[2] < 0 ) {
+      Warning( Here(here), "Illegal negative value for detector dimension. "
+	       "Taking absolute. Check database." );
+    }
+    fSize[0] = 0.5 * TMath::Abs(size[0]);
+    fSize[1] = 0.5 * TMath::Abs(size[1]);
+    fSize[2] = TMath::Abs(size[2]);
   }
 
   return 0;
