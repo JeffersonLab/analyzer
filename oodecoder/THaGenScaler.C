93640b5959e973d6bcb78c3319c6d51e76c30af9
rwmichaels <rom@jlab.org>
2014-06-09 15:04:41 -0400

    add a Scaler base class.  Well, at least it compiles

---
+++ oodecoder/THaGenScaler.C
@@ -0,0 +1,173 @@
+////////////////////////////////////////////////////////////////////
+//
+//   THaGenScaler
+//   A generic scaler
+//
+/////////////////////////////////////////////////////////////////////
+
+#include "THaGenScaler.h"
+#include "THaEvData.h"
+#include "TMath.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+
+THaGenScaler::THaGenScaler(Int_t crate, Int_t slot, Int_t numchan) : ToyModule(crate, slot) { 
+  fHasClock = kFALSE;
+  fFirstTime = kTRUE;
+  fClockChan = -1;
+  fClockRate = 0;
+  fNormScaler = 0;  
+  fDeltaT = DEFAULT_DELTAT;  // a default time interval between readings
+  fWordsExpect = numchan;  
+  fDataArray = new Int_t[fWordsExpect];
+  fPrevData = new Int_t[fWordsExpect];
+  fRate = new Double_t[fWordsExpect];
+  memset(fDataArray, 0, fWordsExpect*sizeof(Int_t));
+  memset(fPrevData, 0, fWordsExpect*sizeof(Int_t));
+  memset(fRate, 0, fWordsExpect*sizeof(Double_t));
+}
+
+
+THaGenScaler::~THaGenScaler() { 
+  delete [] fDataArray;
+  delete [] fPrevData;
+  delete [] fRate;
+}
+
+Int_t THaGenScaler::SetClock(Double_t deltaT, Int_t clockchan, Double_t clockrate) {
+  // Sets teh clock for the time base
+  // retcode:
+  //     0   nothing wrong, but has no deltaT nor clock data. (a bit odd)
+  //    -1   something wrong, see error print
+  //     1   clock rate set for this scaler.  This scaler has a clock.
+  //     2   using deltaT, instead, for an approx time diff between readings
+
+    Int_t retcode = 0;
+    fHasClock = kFALSE;
+    if (clockrate > 0) {
+       if (fNormScaler) { 
+          cout << "THaGenScaler:: WARNING:  Declaring this object to have"<<endl;
+          cout << "   a clock even though his also has a normalization scaler ?"<<endl;
+          cout << "  This makes no sense. "<<endl;
+          retcode = -1;
+       }
+       fHasClock = kTRUE;
+    }
+    if (clockchan < 0 || clockchan >= fWordsExpect) {
+       cout << "THaGenScaler:: ERROR:  clock channel out of range "<<endl;
+       retcode = -1;
+    } else { 
+       fClockChan = clockchan;
+    }
+    fClockRate = clockrate;
+    if (retcode >= 0 && fClockRate > 0) retcode=1;
+    if (deltaT > 0) {
+       fDeltaT = deltaT;    
+       if (fClockRate <= 0 && retcode>=0) retcode=2;
+    } else { 
+      cout << "THaGenScaler:: Warning: using default deltaT = "<<fDeltaT<<endl;
+    }
+    if (retcode == 0) cout << "THaGetScaler:: Warning:: no deltaT nor clockrate define for this scaler "<<endl;
+    return retcode;
+}
+
+void THaGenScaler::LoadNormScaler(THaGenScaler *scal) {
+  if (fHasClock) { 
+    cout << "THaGenScaler:: WARNING:  loading norm scaler although this"<<endl;
+    cout << "   object has a clock ?   This makes no sense. "<<endl;
+  }
+  if ( !scal ) {
+    cout << "THaGenScaler:: ERROR:  attempting to load a non-existent "<<endl;
+    cout << "normalization scaler !" <<endl;
+    return;
+  }    
+  fNormScaler = scal;
+}
+
+Int_t THaGenScaler::Decode(const Int_t *evbuffer) {
+  // Return arg:
+  //     0 = did not decode this module since slot not found yet.
+  //     1 = slot found, decode finished.
+  Int_t result=0;
+  Int_t doload=0;
+  if (IsDecoded()) return result;
+  if (fFirstTime) {
+    fFirstTime = kFALSE;
+  } else {
+    doload=1;
+    memcpy(fPrevData, fDataArray, fWordsExpect*sizeof(Int_t));
+  }
+  if (IsSlot(*evbuffer)) {
+    result=1;
+    fIsDecoded = kTRUE;
+    for (Int_t i=0; i<fWordsExpect; i++) {
+      fDataArray[i] = *(evbuffer+i+1);
+    }
+  }
+  if (doload) LoadRates();
+  return result;
+}
+
+Double_t THaGenScaler::GetTimeSincePrev() {
+// Time since previous reading.
+// If a normalization scaler was defined, use its time base.
+// Otherwise, if this scaler has a clock, use it to get the time precisely.
+// Finally, if there is no clock, use fDeltaT as an approximate time.
+  
+  if (fNormScaler) return fNormScaler->GetTimeSincePrev();
+  Double_t dtime = 0;
+  if (IsDecoded() && fHasClock && fClockRate>0 && checkchan(fClockChan)) {
+    dtime = (fDataArray[fClockChan]-fPrevData[fClockChan])/fClockRate;
+  } else {
+    if (fDeltaT > 0) dtime = fDeltaT;   // default
+  }
+  return dtime;
+}
+
+void THaGenScaler::LoadRates() {
+  if (IsDecoded()) {
+    Double_t dtime = GetTimeSincePrev();
+    if (dtime==0) {
+       memset(fRate, 0, fWordsExpect*sizeof(Double_t));
+       return;
+    }
+    for (Int_t i=0; i<fWordsExpect; i++) {
+      fRate[i] = (fDataArray[i]-fPrevData[i])/dtime;
+    }
+  }
+}
+
+Int_t THaGenScaler::GetData(Int_t chan) {
+  if (checkchan(chan)) {
+    return fDataArray[chan];
+  } else {
+    return 0;
+  }
+}
+
+Double_t THaGenScaler::GetRate(Int_t chan) {
+  if (checkchan(chan)) {
+    return fRate[chan];
+  } else {
+    return 0;
+  }
+}
+
+Bool_t THaGenScaler::IsSlot(Int_t rdata) {
+  Bool_t result;
+  Int_t numchan;
+  result = ((rdata & fHeaderMask)==fHeader);
+  if (result) {
+    numchan = rdata&fNumChanMask;
+    if (numchan != fWordsExpect) 
+         cout << "THaGenScaler:: ERROR:  Inconsistent number of chan"<<endl;
+  }
+  return result;
+}
+
+
+ClassImp(THaGenScaler)
---
+++ oodecoder/THaGenScaler.h
@@ -0,0 +1,58 @@
+#ifndef THaGenScaler_
+#define THaGenScaler_
+
+/////////////////////////////////////////////////////////////////////
+//
+//   THaGenScaler
+//   Generic scaler
+//
+/////////////////////////////////////////////////////////////////////
+
+#include <string>
+#include <map>
+#include <vector>
+#include "Rtypes.h"
+#include "ToyModule.h"
+
+const int DEFAULT_DELTAT = 4;
+
+class THaGenScaler : public ToyModule {
+
+public:
+
+   THaGenScaler() {};  
+   THaGenScaler(Int_t crate, Int_t slot, Int_t numchan);  
+   virtual ~THaGenScaler();  
+
+   Int_t SetClock(Double_t deltaT, Int_t clockchan=0, Double_t clockrate=0);
+
+   Bool_t IsSlot(Int_t rdata);
+   Int_t Decode(const Int_t *evbuffer);
+   Int_t GetData(Int_t chan);
+   Double_t GetRate(Int_t chan);
+   Double_t GetTimeSincePrev();  // returns deltaT since last reading
+   void Clear(const Option_t *opt) { fIsDecoded=kFALSE; };
+   Bool_t IsDecoded() { return fIsDecoded; };
+   void LoadNormScaler(THaGenScaler *scal);
+
+protected:
+
+   void LoadRates();
+
+   Bool_t checkchan(Int_t chan) { return (chan >=0 && chan < fWordsExpect); }
+   static TypeIter_t fgThisType;
+   Bool_t fIsDecoded, fFirstTime, fDeltaT;
+   Int_t *fDataArray, *fPrevData;
+   Double_t *fRate;
+   Int_t fClockChan;
+   Bool_t fHasClock;
+   Double_t fClockRate;
+   THaGenScaler *fNormScaler;
+
+   Int_t fNumChanMask;
+   Int_t slotmask, chanmask, datamask;
+   ClassDef(THaGenScaler,0)  //   A generic scaler
+
+};
+
+#endif
