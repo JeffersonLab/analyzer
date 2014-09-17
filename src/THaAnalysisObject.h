e0c563cf7b46ff31119c8845557a400bb764c016
Ole Hansen <ole@jlab.org>
2014-09-17 01:49:37 -0400

    Fix bug introduced Sep 3, 2014: THaAnalysisObject needs to implement Clear()
    I had overlooked that the base class, TNamed, implements its own Clear,
    which clears name and title of the object. Not a good default. Oops.

--- src/THaAnalysisObject.h
+++ src/THaAnalysisObject.h
@@ -39,6 +39,7 @@ public:
   virtual ~THaAnalysisObject();
   
   virtual Int_t        Begin( THaRunBase* r=0 );
+  virtual void         Clear( Option_t* ="" ) {} // override TNamed::Clear()
   virtual Int_t        End( THaRunBase* r=0 );
   virtual const char*  GetDBFileName() const;
           const char*  GetConfig() const         { return fConfig.Data(); }
