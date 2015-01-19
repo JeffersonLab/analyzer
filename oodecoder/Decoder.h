96f583d822c125371b361e15d9abd9821b5e6fae
rwmichaels <rom@jlab.org>
2015-01-19 14:04:05 -0500

    Add F1TDC and rearrange comments

--- oodecoder/Decoder.h
+++ oodecoder/Decoder.h
@@ -3,19 +3,22 @@
 
 namespace Decoder {
   class THaEvData;            // abstract decoder
-  class Module;               // abstract module   (new)
-  class VmeModule;            // abstract VME module  (new)
-  class FastbusModule;        // abstract Fastbus module  (new)
-  class GenScaler;            // abstract general scaler  (new)
-  class THaCodaDecoder;       // older decoder, not touched 
+  class Module;               // abstract module  
+  class VmeModule;            // abstract VME module  
+  class FastbusModule;        // abstract Fastbus module  
+  class GenScaler;            // abstract general scaler  
+  class THaCodaDecoder;       // older decoder, obsolescent 
   class THaSlotData;          // modified to use modules
-  class THaCrateMap;          // used and unmodified
-  class THaUsrstrutils;       // used and unmodified
-  class THaCodaData;          // used and unmodified
-  class THaCodaFile;          // used and unmodified
-  class THaEtClient;          // used and unmodified
-  class CodaDecoder;          // OO decoder; this and the rest are all new
-  class Lecroy1875Module;     // the rest are specific modules
+  class THaEpics;
+  class EpicsChan;
+  class THaFastBusWord;
+  class THaCrateMap;          
+  class THaUsrstrutils;       
+  class THaCodaData;          
+  class THaCodaFile;          
+  class THaEtClient;          
+  class CodaDecoder;          // OO decoder; this and the following are new
+  class Lecroy1875Module;     
   class Lecroy1877Module;
   class Lecroy1881Module;
   class Scaler560;
@@ -23,7 +26,7 @@ namespace Decoder {
   class Scaler3800;
   class Scaler3801;
   class Fadc250Module;
-  // will need a few more module classes like 1191, F1TDC, etc.
+  class F1TDCModule;
 }
 
 #endif
