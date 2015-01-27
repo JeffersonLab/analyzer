08e74354e2e6465dfae4c0ff2829ce1b6c45fb33
rwmichaels <rom@jlab.org>
2015-01-27 14:44:45 -0500

    add the 2 event handlers, and not the toy handler

--- src/HallA_LinkDef.h
+++ src/HallA_LinkDef.h
@@ -126,11 +126,8 @@
 #pragma link C++ class THaQWEAKHelicity+;
 #pragma link C++ class THaQWEAKHelicityReader+;
 #pragma link C++ class THaQWEAKHelicityReader::ROCinfo+;
-
 #pragma link C++ class THaEvtTypeHandler+;
-#pragma link C++ class ToyPhysicsEvtHandler+;
 #pragma link C++ class THaScalerEvtHandler+;
-
 
 #ifdef ONLINE_ET
 #pragma link C++ class THaOnlRun+;
