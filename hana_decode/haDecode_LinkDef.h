dd47a2bde1cc645966f3fb2c3130a2e637a98b5e
rwmichaels <rom@jlab.org>
2015-01-27 16:33:50 -0500

    add OO decode modules

--- hana_decode/haDecode_LinkDef.h
+++ hana_decode/haDecode_LinkDef.h
@@ -4,17 +4,32 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class THaCodaData+;
-#pragma link C++ class THaCodaFile+;
-#pragma link C++ class THaCrateMap+;
-#pragma link C++ class THaEpics+;
-#pragma link C++ class THaEvData+;
-#pragma link C++ class THaFastBusWord+;
-#pragma link C++ class THaSlotData+;
-#pragma link C++ class THaUsrstrutils+;
-#pragma link C++ class THaCodaDecoder+;
+#pragma link C++ class Decoder::CodaDecoder+;
+#pragma link C++ class Decoder::Module+;  
+#pragma link C++ class Decoder::FastbusModule+;  
+#pragma link C++ class Decoder::VmeModule+;  
+#pragma link C++ class Decoder::GenScaler+;  
+#pragma link C++ class Decoder::Lecroy1877Module+;  
+#pragma link C++ class Decoder::Lecroy1881Module+;  
+#pragma link C++ class Decoder::Lecroy1875Module+;  
+#pragma link C++ class Decoder::Scaler1151+;  
+#pragma link C++ class Decoder::Scaler3800+;  
+#pragma link C++ class Decoder::Scaler3801+;  
+#pragma link C++ class Decoder::Scaler560+;  
+#pragma link C++ class Decoder::Fadc250Module+;  
+#pragma link C++ class Decoder::THaCodaData+;
+#pragma link C++ class Decoder::THaCodaFile+;
+#pragma link C++ class Decoder::THaCrateMap+;
+#pragma link C++ class Decoder::THaEpics+;
+#pragma link C++ class Decoder::THaEvData+;
+#pragma link C++ class Decoder::THaFastBusWord+;
+#pragma link C++ class Decoder::THaSlotData+;
+#pragma link C++ class Decoder::THaUsrstrutils+;
+#pragma link C++ class Decoder::THaCodaDecoder+;
+
 #pragma link C++ class THaBenchmark+;
-#pragma link C++ class THaEvData::RocDat_t+;
+
+#pragma link C++ class Podd::THaEvData::RocDat_t+;
 #pragma link C++ class Podd::MCHitInfo+;
 #pragma link C++ class Podd::MCTrack+;
 #pragma link C++ class Podd::MCTrackPoint+;
