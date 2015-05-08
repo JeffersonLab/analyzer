bc695c4aec47af025e063ee009ed34bb509878a5
Bob Michaels <rom@jlab.org>
2015-05-08 11:29:50 -0400

    Add Decode method to make SkeletonModule class not abstract

--- hana_decode/SkeletonModule.h
+++ hana_decode/SkeletonModule.h
@@ -43,6 +43,7 @@ public:
    virtual Int_t GetData(Int_t chan) const;
    virtual void Init();
    virtual void Clear(const Option_t *opt);
+   Int_t Decode(const UInt_t *p) { return 0; };
 
 #ifdef LIKEV792
 // Loads slot data.  if you don't define this, the base class's method is used
