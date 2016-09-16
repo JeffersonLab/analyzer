9f4cac4bacb2683fe51fd7ecd64dc0ddb49c787b
Stephen A. Wood <saw@jlab.org>
2016-09-16 10:42:25 -0400

    Add decoder modules for CAEN 1190 TDC

---
+++ hana_decode/Caen1190Module.h
@@ -0,0 +1,45 @@
+#ifndef Caen1190Module_
+#define Caen1190Module_
+
+/////////////////////////////////////////////////////////////////////
+//
+//   Caen1190Module
+//
+/////////////////////////////////////////////////////////////////////
+
+
+#include "VmeModule.h"
+
+namespace Decoder {
+
+class Caen1190Module : public VmeModule {
+
+public:
+
+   Caen1190Module() {};
+   Caen1190Module(Int_t crate, Int_t slot);
+   virtual ~Caen1190Module();
+
+   using Module::GetData;
+
+   virtual Int_t GetData(Int_t chan, Int_t hit) const;
+   virtual void Init();
+   virtual void Clear(const Option_t *opt);
+   virtual Int_t Decode(const UInt_t *p) { return 0; };
+
+// Loads slot data.  if you don't define this, the base class's method is used
+  virtual Int_t LoadSlot(THaSlotData *sldat,  const UInt_t *evbuffer, const UInt_t *pstop );
+
+private:
+
+   Int_t *fNumHits;
+   Int_t *fTdcData;  // Raw data	
+
+   static TypeIter_t fgThisType;
+   ClassDef(Caen1190Module,0)  //  Caen1190 of a module; make your replacements
+
+};
+
+}
+
+#endif
