d31887f807af85b169b0f1917007923fc85817f9
Eric Pooser <pooser@jlab.org>
2016-11-11 16:27:32 -0500

    Change MAXBANK to handle larger bank (module) numbers

--- hana_decode/Decoder.h
+++ hana_decode/Decoder.h
@@ -32,7 +32,7 @@ namespace Decoder {
   class SkeletonModule;
 
   static const Int_t MAXROC = 32;
-  static const Int_t MAXBANK = 9999;
+  static const Int_t MAXBANK = 4095;
   static const Int_t MAXSLOT = 30;
   static const Int_t MAXSLOT_FB = 26;
 
