f5398eb96a7dd75689327bbea386e12ca8d905ec
Eric Pooser <pooser@jlab.org>
2016-11-11 16:19:36 -0500

    Modify CAEN1190 decoder to handle bank structures
    Create tst1190 script for decoding raw 1190 TDC data
    
    Modify Makefile to include tst1190 script

---
+++ hana_decode/tst1190_main.C
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cstdio>
+#include <numeric>
+#include <vector>
+#include <time.h>
+#include "Decoder.h"
+#include "THaCodaFile.h"
+#include "CodaDecoder.h"
+#include "THaEvData.h"
+#include "Module.h"
+#include "Caen1190Module.h"
+#include "evio.h"
+#include "THaSlotData.h"
+#include "TString.h"
+#include "TROOT.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TProfile.h"
+#include "TNtuple.h"
+#include "TDirectory.h"
+#include "TGraph.h"
+#include "TVector.h"
+#include "TMultiGraph.h"
+#include "TRandom3.h"
+#include "TCanvas.h"
+
+//#define DEBUG
+//#define WITH_DEBUG
+
+#define CRATE1 1         // HMS single arm setup
+#define SLOTMIN 3
+#define NUMSLOTS 21
+#define NTDCCHAN 128
+#define NUMEVENTS 25
+#define NUMRAWEVENTS 25
+
+using namespace std;
+using namespace Decoder;
+
+TFile *hfile;
+TDirectory *slot_dir[NUMSLOTS], *chan_dir[NTDCCHAN];
+TH1I *h_rawtdc[NUMSLOTS][NTDCCHAN], *h_occupancy[NUMSLOTS];
+
+void GeneratePlots(uint32_t islot, uint32_t chan) {
+  // Slot directory
+  slot_dir[islot] = dynamic_cast <TDirectory*> (hfile->Get(Form("slot_%d", islot)));
+  if (!slot_dir[islot]) {slot_dir[islot] = hfile->mkdir(Form("slot_%d", islot)); slot_dir[islot]->cd();}
+  else hfile->cd(Form("/slot_%d", islot)); 
+  // Histos
+  if (!h_occupancy[islot]) h_occupancy[islot] = new TH1I("h_occupancy", Form("CAEN1190 TDC Occupancy Slot %d", islot), 128, 0, 127);
+  // Channel directory
+  chan_dir[chan] = dynamic_cast <TDirectory*> (slot_dir[islot]->Get(Form("chan_%d", chan)));
+  if (!chan_dir[chan]) {chan_dir[chan] = slot_dir[islot]->mkdir(Form("chan_%d", chan)); chan_dir[chan]->cd();}
+  else hfile->cd(Form("/slot_%d/chan_%d", islot, chan));
+  // Histos
+  if (!h_rawtdc[islot][chan]) h_rawtdc[islot][chan] = new TH1I("h_rawtdc", Form("CAEN1190 RAW TDC Slot %d Channel %d", islot, chan), 4096, 0, 4095);
+}
+
+int main(int argc, char* argv[])
+{
+  // Initialize the analysis clock
+  clock_t t;
+  t = clock();
+
+  // Define the data file to be analyzed
+  TString filename("snippet.dat");
+
+  // Define the analysis debug output
+  ofstream *debugfile = new ofstream;;
+  debugfile->open ("tst1190_main_debug.txt");
+  
+  // Initialize the CODA decoder
+  THaCodaFile datafile(filename);
+  THaEvData *evdata = new CodaDecoder();
+
+  // Initialize the evdata debug output
+  evdata->SetDebug(1);
+  evdata->SetDebugFile(debugfile);
+
+  // Initialize root and output
+  TROOT fadcana("tst1190root", "Hall C analysis");
+  hfile = new TFile("tst1190.root", "RECREATE", "1190 module data");
+
+  // Loop over events
+  cout << "***************************************" << endl;
+  cout << NUMEVENTS << " events will be processed" << endl;
+  cout << "***************************************" << endl;
+  uint32_t iievent = 1;
+  //for(uint32_t ievent = 0; ievent < NUMEVENTS; ievent++) {
+  for(uint32_t ievent = 0; ievent < iievent; ievent++) {
+    // Read in data file
+    int status = datafile.codaRead();
+    if (status == S_SUCCESS) {
+      UInt_t *data = datafile.getEvBuffer();
+      evdata->LoadEvent(data);
+
+      if (debugfile) *debugfile << "****************" << endl;
+      if (debugfile) *debugfile << "Event Number = " << evdata->GetEvNum() << endl;
+      if (debugfile) *debugfile << "****************\n" << endl;
+
+      // Loop over slots
+      for(uint32_t islot = SLOTMIN; islot < NUMSLOTS; islot++) {
+	if (evdata->GetNumRaw(CRATE1, islot) != 0) {  // HMS Single arm setup
+	  Caen1190Module *tdc = NULL;
+	  tdc = dynamic_cast <Caen1190Module*> (evdata->GetModule(CRATE1, islot));   // HMS single arm setup
+	  if (tdc != NULL) {
+	    if (debugfile) *debugfile << "\n///////////////////////////////\n"
+				      << "Results for crate " 
+				      << tdc->GetCrate() << ", slot " 
+				      << tdc->GetSlot() << endl;
+				      
+	    if (debugfile) *debugfile << hex << "tdc pointer = " << tdc << "\n" << dec 
+				      << "///////////////////////////////\n" << endl;
+	    
+	    // Loop over channels
+	    for (uint32_t chan = 0; chan < NTDCCHAN; chan++) {
+	      // Generate FADC plots
+	      GeneratePlots(islot, chan);
+	      for (Int_t hitno = 1; hitno < 17; hitno++) {  // Assume for now up-to 16 hits?
+		if (tdc->GetData(chan, hitno) != 0)
+		  h_rawtdc[islot][chan]->Fill(tdc->GetData(chan, hitno));
+	      } // TDC hit loop
+	    }  // TDC channel loop
+	  }  // TDC module found condition
+	  else 
+	    if (debugfile) *debugfile << "TDC MODULE NOT FOUND!!!" << endl;
+	}  // Number raw words condition
+      }  // Slot loop
+    }  // CODA file read status condition
+    if (iievent % 1000 == 0)
+      //if (iievent % 1 == 0)
+      cout << iievent << " events have been processed" << endl;
+    iievent++;
+    if (status == EOF) break;
+  }  // Event loop 
+
+  cout << "***************************************" << endl;
+  cout << iievent - 1 << " events were processed" << endl;
+  cout << "***************************************" << endl;
+  
+  // Write and clode the data file
+  hfile->Write();
+  hfile->Close();
+
+  // Calculate the analysis rate
+  t = clock() - t;
+  printf ("The analysis took %.1f seconds \n", ((float) t) / CLOCKS_PER_SEC);
+  printf ("The analysis event rate is %.1f Hz \n", (iievent - 1) / (((float) t) / CLOCKS_PER_SEC));
+
+}  // main()
