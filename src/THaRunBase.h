54adad645854cca9e95839f50f8a886e78be2508
Ole Hansen <ole@jlab.org>
2017-12-04 22:52:22 -0500

    Update THaRunBase class version (should have been done in commit 8fd79c6)

--- src/THaRunBase.h
+++ src/THaRunBase.h
@@ -94,7 +94,7 @@ protected:
   virtual Int_t ReadDatabase();
   virtual Int_t ReadInitInfo();
 
-  ClassDef(THaRunBase,1)       // Base class for run objects
+  ClassDef(THaRunBase,2)       // Base class for run objects
 };
 
 
