74dcbdeab6294768774a979301feb1e686074dda
Ole Hansen <ole@jlab.org>
2018-07-26 14:37:37 -0400

    Use major version as SOVERSION for locally built EVIO library
    I am making the assumption that EVIO releases with the same
    major version number are compatible and otherwise aren't.
    The DAQ group would have to confirm, although they may not
    even have a policy. Still, this is better than no library
    versioning at all.

--- cmake/Modules/FetchBuildEVIO.cmake
+++ cmake/Modules/FetchBuildEVIO.cmake
@@ -1,5 +1,7 @@
 # Fetch EVIO archive (currently from our GitHub mirror) and prepare for building the C-library
 
+message(STATUS "Will build local copy of EVIO")
+
 set(EVIO_VERSION 4.4.6)
 set(repo hallac_evio)
 set(release evio-${EVIO_VERSION})
@@ -32,5 +34,4 @@ execute_process(COMMAND tar -x --strip-components=3 -f ${EVIO_TARFILE} "*/libsrc
 configure_file(evio/CMakeLists.txt.in ${EVIO_SOURCE_DIR}/CMakeLists.txt @ONLY)
 file(COPY evio/EVIOConfig.cmake.in DESTINATION ${EVIO_SOURCE_DIR})
 
-message(STATUS "Will build local copy of EVIO")
 add_subdirectory(${EVIO_SOURCE_DIR} ${EVIO_BINARY_DIR})
