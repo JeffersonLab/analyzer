JeffersonLab/analyzer

commit d256a36f823efc17e310b2df3aad336036db4025

Do not use globals like gHaVars as default parameters in the public API
Using the globals in this way is fine from a C++ point of view, but the
ROOT C++ interpreter has frequent issues with it when loading scripts.

Podd/THaCut.cxx
@@ -11,6 +11,7 @@
 
 #include "THaCut.h"
 #include "THaPrintOption.h"
+#include "THaGlobals.h"
 #include "TMath.h"
 
 #include <iostream>
@@ -43,6 +44,10 @@ THaCut::THaCut( const char* name, const char* expression, const char* block,
   // expressions would get reparsed instead of queried. This wouldn't
   // work properly with a non-default array evaluation mode (OR/XOR).
 
+  if( vlst )
+    vlst = gHaVars;
+  if( !clst )
+    clst = gHaCuts;
   SetList(vlst);
   SetCutList(clst);
 

Podd/THaVform.cxx
@@ -16,6 +16,7 @@
 #include "THaString.h"
 #include "THaVarList.h"
 #include "THaCut.h"
+#include "THaGlobals.h"
 #include "TTree.h"
 
 #include <iostream>
@@ -32,6 +33,10 @@ THaVform::THaVform( const char *type, const char* name, const char* formula,
     fPrefix(kNoPrefix)
 {
   SetName(name);
+  if( vlst )
+    vlst = gHaVars;
+  if( !clst )
+    clst = gHaCuts;
   SetList(vlst);
   SetCutList(clst);
   string stemp1 = StripPrefix(formula);

Podd/THaCut.h
@@ -14,7 +14,7 @@ class THaCut : public THaFormula {
 public:
   THaCut();
   THaCut( const char* name, const char* expression, const char* block,
-	  const THaVarList* vlst = gHaVars, const THaCutList* clst = gHaCuts );
+	  const THaVarList* vlst=nullptr, const THaCutList* clst=nullptr );
   THaCut( const THaCut& ) = default;
   THaCut& operator=( const THaCut& ) = default;
   virtual ~THaCut() = default;

Podd/THaFormula.cxx
@@ -18,6 +18,7 @@
 #include "THaVarList.h"
 #include "THaCutList.h"
 #include "THaCut.h"
+#include "THaGlobals.h"
 #include "TROOT.h"
 #include "TError.h"
 #include "TVirtualMutex.h"
@@ -82,6 +83,11 @@ THaFormula::THaFormula( const char* name, const char* expression,
   // the call to Compile(). Not only do we have our own Compile(), but
   // also out own DefinedVariable() etc. virtual functions. A disturbing
   // design error of the ROOT base class indeed.
+
+  if( !fVarList )
+    fVarList = gHaVars;
+  if( !fCutList )
+    fCutList = gHaCuts;
 
   if( Init( name, expression ) != 0 ) {
     RegisterFormula(false);

Podd/THaFormula.h
@@ -9,7 +9,6 @@
 
 #include "RVersion.h"
 #include "v5/TFormula.h"
-#include "THaGlobals.h"
 #include <vector>
 #include <iostream>
 
@@ -26,7 +25,7 @@ public:
 
   THaFormula();
   THaFormula( const char* name, const char* formula, Bool_t do_register=true,
-	      const THaVarList* vlst=gHaVars, const THaCutList* clst=gHaCuts );
+	      const THaVarList* vlst=nullptr, const THaCutList* clst=nullptr );
   THaFormula( const THaFormula& rhs );
   THaFormula& operator=( const THaFormula& rhs );
   virtual ~THaFormula();

Podd/THaVform.h
@@ -29,7 +29,7 @@ public:
     fData(0), fType(kUnknown), fDebug(0), fVarPtr(nullptr), fOdata(nullptr),
     fPrefix(0) {}
   THaVform( const char* type, const char* name, const char* formula,
-      const THaVarList* vlst=gHaVars, const THaCutList* clst=gHaCuts );
+      const THaVarList* vlst=nullptr, const THaCutList* clst=nullptr );
   virtual  ~THaVform();
   THaVform(const THaVform& vform);
   THaVform& operator=(const THaVform& vform);

commit ca9cc27df42c412d9030f8bf8a041af61fd51ada

Bugfix: No global variable list defined in THaCut and THaVform.
This was a regression introduced with commit d256a36f (9-Nov-23).
Just a silly typo.

Podd/THaCut.cxx
@@ -44,7 +44,7 @@ THaCut::THaCut( const char* name, const char* expression, const char* block,
   // expressions would get reparsed instead of queried. This wouldn't
   // work properly with a non-default array evaluation mode (OR/XOR).
 
-  if( vlst )
+  if( !vlst )
     vlst = gHaVars;
   if( !clst )
     clst = gHaCuts;

Podd/THaVform.cxx
@@ -33,7 +33,7 @@ THaVform::THaVform( const char *type, const char* name, const char* formula,
     fPrefix(kNoPrefix)
 {
   SetName(name);
-  if( vlst )
+  if( !vlst )
     vlst = gHaVars;
   if( !clst )
     clst = gHaCuts;