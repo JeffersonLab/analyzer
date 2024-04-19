JeffersonLab/analyzer

commit ccbc82d2994144678eccec7260f9426bd3c95a57

Rework time zone handling. Make default time zone configurable.
Currently, the default time zone is used both for database time stamps
without an explicit GMT offset and for TDatime objects (representing
run start times). With a future upgrade, we can eliminate the latter
usage by switching to TTimeStamp objects, but the former will remain.

Database/Database.cxx
@@ -78,6 +78,52 @@ const char* Here( const char* method, const char* prefix )
 //=============================================================================
 namespace Podd {
 
+namespace {
+
+const char* gDefaultTZString = "US/Eastern";
+
+//_____________________________________________________________________________
+Long64_t GetOffsetToLocal(const char* tz)
+{
+  // Returns the offset in seconds between the given time zone 'tz' and
+  // the time zone of the local machine
+  time_t tloc = time(nullptr);
+  struct tm tml{}, tmd{};
+  localtime_r(&tloc, &tml);
+
+  const char* cur_tz = gSystem->Getenv("TZ");
+  gSystem->Setenv("TZ", tz);
+  localtime_r(&tloc, &tmd);
+  if( cur_tz )
+    gSystem->Setenv("TZ", cur_tz);
+  else
+    gSystem->Unsetenv("TZ");
+
+  return tmd.tm_gmtoff - tml.tm_gmtoff;
+}
+
+//_____________________________________________________________________________
+TString MkDefaultTZ()
+{
+  gNeedTZCorrection = (GetOffsetToLocal(gDefaultTZString) != 0);
+  return gDefaultTZString;
+}
+
+} // namespace
+
+TString gDefaultTZ = MkDefaultTZ();
+Bool_t  gNeedTZCorrection = true;
+
+//_____________________________________________________________________________
+void SetDefaultTZ( const char* tz )
+{
+  if( !tz || !*tz ) {
+    tz = gDefaultTZString;
+  }
+  gNeedTZCorrection = (GetOffsetToLocal(tz) != 0);
+  gDefaultTZ = tz;
+}
+
 //_____________________________________________________________________________
 TString& GetObjArrayString( const TObjArray* array, Int_t i )
 {
@@ -291,12 +337,12 @@ static thread_local string loaddb_prefix; // Actual prefix of object in LoadDB (
 // Local helper functions
 namespace {
 //_____________________________________________________________________________
-Int_t IsDBdate( const string& line, TDatime& date, bool warn = true )
+Int_t IsDBdate( const string& line, Long64_t& date, bool warn = true )
 {
   // Check if 'line' contains a valid database time stamp. If so,
   // parse the line, set 'date' to the extracted time stamp, and return 1.
   // Else return 0;
-  // Time stamps must be in SQL format: [ yyyy-mm-dd hh:mi:ss ]
+  // Time stamps must be in SQL format: [ yyyy-mm-dd hh:mi:ss gmtoffset]
 
   auto lbrk = line.find('[');
   if( lbrk == string::npos || lbrk >= line.size() - 12 ) return 0;
@@ -306,47 +352,61 @@ Int_t IsDBdate( const string& line, TDatime& date, bool warn = true )
 
   bool err = false;
   struct tm tmc{};
+  time_t ldate = 0;
   const char* c = strptime(ts.c_str(), " %Y-%m-%d %H:%M:%S %z ", &tmc);
-  if( !c || c != ts.c_str()+ts.size() ) {
-    // No time zone field: try again without that field.
-    // We allow this for compatibility with old databases.
-    c = strptime(ts.c_str(), " %Y-%m-%d %H:%M:%S ", &tmc);
-    if( !c || c != ts.c_str()+ts.size() )
-      err = true;
-    // If this succeeds, assume the value represents local time.
-    // This may cause errors if the database and the local time zone differ!
-  }
+  if( c && c == ts.c_str()+ts.size() ) {
 #ifdef __GLIBC__
-  else {
-    // Time zone offset explicitly given. If using glibc, strptime does not
-    // apply the time zone offset to the result, so we need to explicitly
-    // convert it to localtime, which is what TDatime wants.
-    // Get localtime GMT offset at the specified date
+    // With glibc, strptime does not apply the given time zone offset to the
+    // result. It parses the offset and fills the gmtoff field, but does not
+    // adjust tm_hour/tm_min accordingly. In short, handling of the %z field is
+    // currently not fully implemented in glibc. So we need to do this by hand.
     struct tm tml = tmc;
     tml.tm_isdst = -1;
-    time_t tval = mktime(&tml);
-    if( tval != -1 ) {
+    // Get localtime GMT offset at the specified date.
+    ldate = mktime(&tml);
+    if( ldate != -1 ) {
+      // tmc holds the GMT offset from the parsed string (%z).
+      // tml holds the GMT offset of the local system time zone.
+      // tmc.tm_{hour,min} come directly from the parsed string w/o adjustments
+      // mktime ignores the tm_gmtime field and interprets the time as local
+      // time. However, the time is actually given not in the local time zone,
+      // but as having tml.tm_gmtoff GMT offset. So we need to shift the parsed
+      // time from that offset to the GMT offset of the local time zone. Then
+      // mktime sees actual local time and returns the correct Unix time.
       long tzdiff = tml.tm_gmtoff - tmc.tm_gmtoff;
       if( tzdiff != 0 ) {
         tmc.tm_hour += (int) (tzdiff / 3600);
         tmc.tm_min += (int) ((tzdiff % 3600) / 60);
         tmc.tm_isdst = tml.tm_isdst;
-        tval = mktime(&tmc);  // Normalizes tmc, converts to localtime
-        if( tval == -1 )
-          err = true;
+        ldate = mktime(&tmc);  // Normalizes tmc (e.g. hour >= 24 wraps, etc.)
       }
-    } else {
-      err = true;
     }
-  }
+#else
+    // This just works on macOS
+    ldate = mktime(&tmc);
 #endif
+  } else {
+    // No time zone field: try again without that field.
+    // We allow this for compatibility with old databases.
+    WithDefaultTZ(
+      c = strptime(ts.c_str(), " %Y-%m-%d %H:%M:%S ", &tmc);
+      if( c && c == ts.c_str()+ts.size() ) {
+        // If this succeeds, assume the value represents time in the
+        // default timezone
+        tmc.tm_isdst = -1;
+        ldate = mktime(&tmc);
+      } else
+        err = true;
+    )
+  }
+  if( ldate == -1 )
+    err = true;
   if( err || tmc.tm_year < 95 ) {
     if( warn )
       ::Warning("IsDBdate()", "Invalid date tag %s", line.c_str());
     return 0;
   }
-  date.Set(tmc.tm_year + 1900, tmc.tm_mon + 1, tmc.tm_mday,
-           tmc.tm_hour, tmc.tm_min, tmc.tm_sec);
+  date = ldate;
   errno = 0;  // strptime() may set errno to spurious values despite succeeding
   return 1;
 }
@@ -616,7 +676,7 @@ Bool_t DBDatesDiffer( const TDatime& a, const TDatime& b )
 }
 
 //_____________________________________________________________________________
-Int_t LoadDBvalue( FILE* file, const TDatime& date, const char* key,
+Int_t LoadDBvalue( FILE* file, const TDatime& datime, const char* key,
                    string& value )
 {
   // Load a data value tagged with 'key' from the database 'file'.
@@ -639,7 +699,9 @@ Int_t LoadDBvalue( FILE* file, const TDatime& date, const char* key,
   unique_ptr<char[]> buf{new char[bufsiz]};
   char* const bufp = buf.get();
 
-  TDatime keydate(950101, 0), prevdate(950101, 0);
+  WithDefaultTZ(Long64_t date = datime.Convert());
+  Long64_t keydate = 0, prevdate = 0;
+
   bool found = false, do_ignore = false;
   string dbline;
   vector<string> lines;
@@ -649,6 +711,7 @@ Int_t LoadDBvalue( FILE* file, const TDatime& date, const char* key,
   rewind(file);
   if( errno )
     goto err;
+
 
   while( ReadDBline(file, bufp, bufsiz, dbline) != EOF ) {
     if( dbline.empty() ) continue;
@@ -1222,10 +1285,6 @@ Int_t SeekDBdate( FILE* file, const TDatime& date, Bool_t end_on_tag )
   static const char* const here = "SeekDBdateTag";
 
   if( !file ) return 0;
-  const int LEN = 256;
-  char buf[LEN];
-  TDatime tagdate(950101, 0), prevdate(950101, 0);
-  const bool kNoWarn = false;
 
   errno = 0;
   off_t pos = ftello(file);
@@ -1234,6 +1293,14 @@ Int_t SeekDBdate( FILE* file, const TDatime& date, Bool_t end_on_tag )
       perror(here);
     return 0;
   }
+
+  WithDefaultTZ(Long64_t ldate = date.Convert());
+  Long64_t tagdate = 0, prevdate = 0;
+
+  const bool kNoWarn = false;
+  const int LEN = 256;
+  char buf[LEN];
+
   off_t foundpos = -1;
   bool found = false, quit = false;
   while( !errno && !quit && fgets(buf, LEN, file) ) {
@@ -1242,7 +1309,7 @@ Int_t SeekDBdate( FILE* file, const TDatime& date, Bool_t end_on_tag )
     if( buf[len - 1] == '\n' ) buf[len - 1] = 0; //delete trailing newline
     string line(buf);
     if( IsDBdate(line, tagdate, kNoWarn)
-        && tagdate <= date && tagdate >= prevdate ) {
+        && tagdate <= ldate && tagdate >= prevdate ) {
       prevdate = tagdate;
       foundpos = ftello(file);
       found = true;
@@ -1277,12 +1344,13 @@ Int_t SeekDBdate( istream& istr, const TDatime& date, Bool_t end_on_tag )
 
   if( !istr.good() )
     return 0;
-  TDatime tagdate(950101, 0), prevdate(950101, 0);
-  const bool kNoWarn = false;
-
   auto pos = istr.tellg();
   if( pos == -1 )
     return 0;
+
+  WithDefaultTZ(Long64_t ldate = date.Convert());
+  Long64_t tagdate = 0, prevdate = 0;
+  const bool kNoWarn = false;
 
   string line;
   decltype(pos) foundpos{-1};
@@ -1294,7 +1362,7 @@ Int_t SeekDBdate( istream& istr, const TDatime& date, Bool_t end_on_tag )
     if( line.find_first_not_of(" \t") == string::npos )
       continue;
     if( IsDBdate(line, tagdate, kNoWarn)
-        && tagdate <= date && tagdate >= prevdate ) {
+        && tagdate <= ldate && tagdate >= prevdate ) {
       prevdate = tagdate;
       foundpos = istr.tellg();
       found = true;
@@ -1313,7 +1381,8 @@ Int_t SeekDBdate( istream& istr, const TDatime& date, Bool_t end_on_tag )
 //_____________________________________________________________________________
 Bool_t IsDBtimestamp( const string& line, TDatime& date )
 {
-  return IsDBdate(line, date, true);
+  WithDefaultTZ(Long64_t ldate = date.Convert());
+  return IsDBdate(line, ldate, true);
 }
 
 //_____________________________________________________________________________

Database/Database.h
@@ -64,6 +64,26 @@ Int_t    SeekDBconfig( FILE* file, const char* tag, const char* label = "config"
 Int_t    SeekDBdate( std::istream& istr, const TDatime& date, Bool_t end_on_tag = false );
 Bool_t   IsDBtimestamp( const std::string& line, TDatime& keydate );
 
+// Time zone to assume for legacy database time stamps without time zone offsets.
+// The default is "US/Eastern".
+void     SetDefaultTZ(const char* tz = nullptr);
+extern TString  gDefaultTZ;
+extern Bool_t   gNeedTZCorrection;
+
 }  // namespace Podd
+
+#define WithDefaultTZ(cmd)                   \
+ const char* cur_tz = nullptr;               \
+ if( Podd::gNeedTZCorrection ) {             \
+   cur_tz = gSystem->Getenv("TZ");           \
+   gSystem->Setenv("TZ", Podd::gDefaultTZ);  \
+ }                                           \
+ cmd;                                        \
+ if( Podd::gNeedTZCorrection ) {             \
+   if( cur_tz )                              \
+     gSystem->Setenv("TZ", cur_tz );         \
+   else                                      \
+     gSystem->Unsetenv("TZ");                \
+ }
 
 #endif //Podd_Database_h_

Podd/THaAnalyzer.cxx
@@ -644,9 +644,8 @@ Int_t THaAnalyzer::DoInit( THaRunBase* run )
   // Set run-level info that was retrieved when initializing the run.
   // In case we analyze a continuation segment, fEvtData may not see
   // the event(s) where these data come from (usually Prestart).
-  fEvData->SetRunInfo(run->GetNumber(),
-                      run->GetType(),
-                      run->GetDate().Convert());
+  WithDefaultTZ(ULong64_t run_tloc = run->GetDate().Convert());
+  fEvData->SetRunInfo(run->GetNumber(),run->GetType(), run_tloc);
 
   // Deal with the run.
   bool new_run   = ( !fRun || *fRun != *run );
@@ -736,7 +735,10 @@ Int_t THaAnalyzer::DoInit( THaRunBase* run )
 
   // Tell the decoder the run time. This will trigger decoder
   // initialization (reading of crate map data etc.)
-  fEvData->SetRunTime( run_time.Convert());
+//already done via SetRunInfo above
+//  gSystem->Setenv("TZ", Podd::GetDefaultTZ());
+//  fEvData->SetRunTime( run_time.Convert());
+//  gSystem->Setenv("TZ", cur_tz.Data());
 
   // Tell the decoder about the run's CODA version
   fEvData->SetDataVersion( run->GetDataVersion() );

Podd/THaInterface.cxx
@@ -44,7 +44,7 @@ THaDB*       gHaDB       = nullptr;  // Database system to use
 
 THaInterface* THaInterface::fgAint = nullptr;  // Pointer to this interface
 
-static TString fgTZ;
+static const char* fgTZ = nullptr;
 
 //_____________________________________________________________________________
 THaInterface::THaInterface( const char* appClassName, int* argc, char** argv,
@@ -114,21 +114,8 @@ THaInterface::THaInterface( const char* appClassName, int* argc, char** argv,
     ss = s(re);
   }
 
-  // Because of lack of foresight, the analyzer uses TDatime objects,
-  // which are kept in localtime() and hence are not portable, and also
-  // uses localtime() directly in several places. As a result, database
-  // lookups depend on the timezone of the machine that the replay is done on!
-  // If this timezone is different from the one in which the data were taken,
-  // mismatches may occur. This is bad.
-  // FIXME: Use TTimeStamp to keep time in UTC internally.
-  // To be done in version 1.6
-  //
-  // As a temporary workaround, we assume that all data were taken in
-  // US/Eastern time, and that the database has US/Eastern timestamps.
-  // This is certainly true for all JLab production data..
+  // Save user's TZ. Podd::Database temporarily overrides this.
   fgTZ = gSystem->Getenv("TZ");
-  gSystem->Setenv("TZ","US/Eastern");
-
 
   fgAint = this;
 }
@@ -140,7 +127,10 @@ THaInterface::~THaInterface()
 
   if( fgAint == this ) {
     // Restore the user's original TZ
-    gSystem->Setenv("TZ",fgTZ.Data());
+    if( fgTZ )
+      gSystem->Setenv("TZ",fgTZ);
+    else
+      gSystem->Unsetenv("TZ");
     // Clean up the analyzer object if defined
     delete THaAnalyzer::GetInstance();
     // Delete all global lists and objects contained in them

Podd/THaRunBase.cxx
@@ -13,6 +13,8 @@
 #include "THaEvData.h"
 #include "DAQconfig.h"
 #include "THaPrintOption.h"
+#include "Database.h"
+#include "TSystem.h"
 #include "TClass.h"
 #include "TError.h"
 #include <iostream>
@@ -142,7 +144,7 @@ Int_t THaRunBase::Update( const THaEvData* evdata )
   if( evdata->IsPrestartEvent() ) {
     fDataRead |= kDate|kRunNumber|kRunType;
     if( !fAssumeDate ) {
-      fDate.Set( evdata->GetRunTime() );
+      WithDefaultTZ(fDate.Set( evdata->GetRunTime() ));
       fDataSet |= kDate;
     }
     SetNumber( evdata->GetRunNum() );
@@ -510,7 +512,7 @@ void THaRunBase::SetDate( UInt_t tloc )
   // Set timestamp of this run to 'tloc' which is in Unix time
   // format (number of seconds since 01 Jan 1970).
 
-  TDatime date( tloc );
+  WithDefaultTZ(TDatime date( tloc ));
   SetDate( date );
 }
 

commit 0e2f1969c45a9563cafba80842706461709f80af

THaCrateMap: Fix non-working time stamp handling
This was a regression introduced in commit ccbc82d2 (16-Nov-23). The
changes introduced there should have also been applied to the
THaCrateMap file reader.

Database/Database.cxx
@@ -1335,7 +1335,17 @@ Int_t SeekDBdate( istream& istr, const TDatime& date, Bool_t end_on_tag )
 //_____________________________________________________________________________
 Bool_t IsDBtimestamp( const string& line, TDatime& date )
 {
-  WithDefaultTZ(Long64_t ldate = date.Convert());
+  Long64_t ldate = -1;
+  Bool_t ret = IsDBdate(line, ldate, true);
+  if( ret && ldate >= 0 ) {
+    WithDefaultTZ(date.Set(ldate));
+  }
+  return ret;
+}
+
+//_____________________________________________________________________________
+Bool_t IsDBtimestamp( const string& line, Long64_t& ldate )
+{
   return IsDBdate(line, ldate, true);
 }
 

Database/Database.h
@@ -62,6 +62,7 @@ Int_t    SeekDBconfig( FILE* file, const char* tag, const char* label = "config"
 
 Int_t    SeekDBdate( std::istream& istr, const TDatime& date, Bool_t end_on_tag = false );
 Bool_t   IsDBtimestamp( const std::string& line, TDatime& keydate );
+Bool_t   IsDBtimestamp( const std::string& line, Long64_t& keydate );
 
 // Time zone to assume for legacy database time stamps without time zone offsets.
 // The default is "US/Eastern".

hana_decode/THaCrateMap.cxx
@@ -288,7 +288,7 @@ int THaCrateMap::init(ULong64_t tloc)
   // 'tloc' is the time-stamp/index into the database's periods of validity.
 
   const char* const here = "THaCrateMap::init(tloc)";
-  fInitTime = tloc;
+  WithDefaultTZ(fInitTime = tloc);
   FILE* fi = Podd::OpenDBFile(fDBfileName.c_str(), fInitTime, here, "r", 1);
   return init(fi, fDBfileName.c_str());
 }
@@ -493,7 +493,8 @@ int THaCrateMap::init(const string& the_map)
   string line; line.reserve(128);
   istringstream s(the_map);
   Int_t found_tscrate=0;
-  TDatime keydate(950101, 0), prevdate(950101, 0);
+  WithDefaultTZ(const Long64_t ldate = fInitTime.Convert());
+  Long64_t keydate = 0, prevdate = 0;
   bool do_ignore = false, in_crate = false;
   int lineno = 0;
 
@@ -514,7 +515,7 @@ int THaCrateMap::init(const string& the_map)
     // To remove a previously-defined crate after a certain time stamp, use
     // crate type "unused".
     if( Podd::IsDBtimestamp(line, keydate) ) {
-      do_ignore = (keydate > fInitTime || keydate < prevdate);
+      do_ignore = (keydate > ldate || keydate < prevdate);
       in_crate = false;
       continue;
     } else if( do_ignore )

hana_decode/THaCrateMap.h
@@ -84,7 +84,7 @@ class THaCrateMap {
      enum ECrateCode { kUnknown, kFastbus, kVME, kScaler, kCamac };
 
      std::string fDBfileName;     // Database file name
-     TDatime     fInitTime;       // Database time stamp
+     TDatime     fInitTime;       // Database time stamp BCI: -> Long64_t
      UInt_t      fTSROC;          // Crate (aka ROC) of the trigger supervisor
 
      class SlotInfo_t {