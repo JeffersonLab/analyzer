e70d1c0d0eb9a69e7aec2a21b85d5606ce8532c6
Ryan Conaway <53782540+Mrc1104@users.noreply.github.com>
2024-07-25 12:00:19 -0400

    THaEtClient Fix (#1)
    * Ports Bryan Moffit's et_consumer example (https://github.com/bmoffit/evet)
    into the THaEtClient class.

--- hana_decode/THaEtClient.h
+++ hana_decode/THaEtClient.h
@@ -76,8 +76,42 @@ private:
     time_t daqt1;
     double ratesum;
 
-    ClassDef(THaEtClient,0)   // ET client connection for online data
+	/* 
+		ET Data de-chunk-ifying.
+		Taken from Bryan Moffit's Repo:
+		https://github.com/bmoffit/evet
+	 */
+	typedef struct etChunkStat
+	{
+		uint32_t *data;
+		size_t length;
+		int32_t endian;
+		int32_t swap;
 
+		int32_t  evioHandle;
+	} etChunkStat_t;
+	typedef struct evetHandle
+	{
+		et_sys_id etSysId;
+		et_att_id etAttId;
+		et_event **etChunk;      // pointer to array of et_events (pe)
+		int32_t  etChunkSize;    // user requested (et_events in a chunk)
+		int32_t  etChunkNumRead; // actual read from et_events_get
+		int32_t  currentChunkID;  // j
+		etChunkStat_t currentChunkStat; // data, len, endian, swap
+		int32_t verbose=1; // 0 (none), 1 (data rate), 2+ (verbose)
+
+	} evetHandle_t ;
+
+	int32_t evetOpen(et_sys_id etSysId, int32_t chunk, evetHandle_t &evh);
+	int32_t evetClose(evetHandle_t &evh);
+	int32_t evetReadNoCopy(evetHandle_t &evh, const uint32_t **outputBuffer, uint32_t *length);
+	int32_t evetGetEtChunks(evetHandle_t &evh);
+	int32_t evetGetChunk(evetHandle_t &evh);
+
+	evetHandle evh;
+
+  ClassDef(THaEtClient,0)   // ET client connection for online data
 };
 
 }
