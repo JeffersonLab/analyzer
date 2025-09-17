JeffersonLab/analyzer

commit ac4da36f3cab1841744197de93ee772f2117ffeb

Decoder: With CODA3, set physics event type to trigger bank event type
Requested by Sanghwa. This makes the event type variable in the event
headers in the ROOT file actually useful.

hana_decode/CodaDecoder.cxx
@@ -196,10 +196,20 @@ Int_t CodaDecoder::LoadEvent( const UInt_t* evbuffer )
     }
   }
 
-  else if( event_type <= MAX_PHYS_EVTYPE && !PrescanModeEnabled() ) {
-    if( fDataVersion == 3 &&
-        (ret = trigBankDecode(evbuffer)) != HED_OK ) {
-      return ret;
+  else if( event_type > 0 && event_type <= MAX_PHYS_EVTYPE &&
+           !PrescanModeEnabled() ) {
+    if( fDataVersion == 3 ) {
+      if( (ret = trigBankDecode(evbuffer)) != HED_OK )
+        return ret;
+      assert( event_type == 1 ); // else bug in InterpretBankTag
+      // Copy the CODA3 "event type" from the trigger bank to event_type
+      // so that physics events do not all trivially have type = 1.
+      // event_type is what goes into each event header in the ROOT file.
+      // Caution: tsEvType comes from a 16-bit word in the data stream
+      // and so could be way larger than MAX_PHYS_EVTYPE, or it could be zero.
+      // That should be safe, though, since nothing in physics_decode appears
+      // to care about this number.
+      event_type = tsEvType;
     }
     ret = physics_decode(evbuffer);
   }
@@ -353,15 +363,23 @@ UInt_t CodaDecoder::InterpretBankTag( UInt_t tag )
       case 0xff70:
         evtyp = 1;      // for CODA 3.* physics events are type 1.
         break;
-      default:          // Undefined CODA 3 event type
-        cerr << "CodaDecoder:: WARNING:  Undefined CODA 3 event type, tag = "
-             << "0x" << hex << tag << dec << endl;
-        evtyp = 0;
-        //FIXME evtyp = 0 could also be a user event type ...
-        // maybe throw an exception here?
+      default:          // Unsupported CODA 3 event type
+        // May need to accommodate, or it really is garbage data
+        cerr << "CodaDecoder:: WARNING:  Unsupported CODA 3 event type, tag = "
+             << "0x" << hex << tag << dec << ". Call expert." << endl;
+        evtyp = 0;      // tells decoder that this is not usable data
     }
   } else {              // User event type
     evtyp = tag;        // ET-insertions
+    if( evtyp <= MAX_PHYS_EVTYPE || evtyp == PRESTART_EVTYPE ||
+        evtyp == GO_EVTYPE || evtyp == END_EVTYPE ) {
+      // FIXME: This is not a CODA3 error, but the result of our internal
+      //  legacy event type numbering. But we can't go with it until we
+      //  rewrite the downstream code
+      cerr << "CodaDecoder:: ERROR:  User event type number " << evtyp
+           << " clashes with a reserved value. Call expert." << endl;
+      evtyp = 0;
+    }
   }
 
   return evtyp;

Podd/CodaRawDecoder.cxx
@@ -35,6 +35,7 @@ CodaRawDecoder::CodaRawDecoder()
         { "evtime",    "Event time",     kULong,  0, &evt_time },
         { "datatype",  "Data type",      kUInt,   0, &data_type },
         { "trigbits",  "Trigger bits",   kUInt,   0, &trigger_bits },
+        { "tsevtyp",   "TS event type",  kUInt,   0, &tsEvType },
         { nullptr }
     };
     TString prefix("g");

commit ca26062bd3b686d2e7911f348454a4d023500770

CodaDecoder: Make physics analysis work with out-of-range TS event types
This was a regression introduced in commit ac4da36 (3/29/25). If the TS
event type is too large (or 0), IsPhysicsTrigger would fail for physics
events.

hana_decode/CodaDecoder.cxx
@@ -205,11 +205,18 @@ Int_t CodaDecoder::LoadEvent( const UInt_t* evbuffer )
       // Copy the CODA3 "event type" from the trigger bank to event_type
       // so that physics events do not all trivially have type = 1.
       // event_type is what goes into each event header in the ROOT file.
+      //
       // Caution: tsEvType comes from a 16-bit word in the data stream
       // and so could be way larger than MAX_PHYS_EVTYPE, or it could be zero.
-      // That should be safe, though, since nothing in physics_decode appears
-      // to care about this number.
-      event_type = tsEvType;
+      // Even though nothing in physics_decode currently cares about this
+      // value, the IsPhysicsTrigger() member function does. To maintain
+      // compatibility, we let event_type = MAX_PHYS_EVTYPE if the tsEvType is
+      // out of range, so IsPhysicsTrigger() still gives the right answer.
+      // FIXME BCI loosen this restriction in v1.8
+      event_type = (tsEvType > 0 && tsEvType <= MAX_PHYS_EVTYPE)
+                   ? tsEvType
+                   : MAX_PHYS_EVTYPE; // a kludgy way to tell the user that
+                                      // this is an out-of-range event type
     }
     ret = physics_decode(evbuffer);
   }