dc073f26c1dcad2cf770e1ce67174e16f19a54ad
Ole Hansen <ole@jlab.org>
2025-09-29 17:50:39 -0400

    Decoder: Add aligned_default_init_allocator (currently unused)

--- hana_decode/CustomAlloc.h
+++ hana_decode/CustomAlloc.h
@@ -6,6 +6,7 @@
 #include <memory>
 
 namespace Decoder {
+//--------------------------------------------------------------------------
 // From https://stackoverflow.com/questions/21028299/is-this-behavior-of-vectorresizesize-type-n-under-c11-and-boost-container/21028912#21028912
 // Allocator adaptor that interposes construct() calls to
 // convert value initialization into default initialization.
@@ -23,7 +24,11 @@ public:
 
   template<typename U>
   void construct( U* ptr )
+#if __cplusplus >= 201703L
+  noexcept(std::is_nothrow_default_constructible_v<U>) {
+#else
   noexcept(std::is_nothrow_default_constructible<U>::value) {
+#endif
     ::new(static_cast<void*>(ptr)) U;
   }
   template<typename U, typename...Args>
@@ -33,6 +38,44 @@ public:
   }
 };
 
+//--------------------------------------------------------------------------
+#if __cplusplus >= 201703L
+template<typename T, size_t L=alignof(T), typename A=std::allocator<T>>
+class aligned_default_init_allocator : public A {
+  typedef std::allocator_traits<A> a_t;
+public:
+  using A::A;
+
+  T* allocate( size_t n ) {
+    return static_cast<T*>(
+      ::operator new(n * sizeof(T), static_cast<std::align_val_t>(L))
+    );
+  }
+
+  void deallocate( T* ptr, size_t ) noexcept {
+    ::operator delete(ptr, static_cast<std::align_val_t>(L));
+  }
+
+  template<typename U> struct
+  rebind {
+    using other =
+      aligned_default_init_allocator<U, L, typename a_t::template rebind_alloc<U>>;
+  };
+
+  template<typename U>
+  void construct( U* ptr )
+  noexcept(std::is_nothrow_default_constructible_v<U>) {
+    ::new(static_cast<void*>(ptr)) U;
+  }
+
+  template<typename U, typename...Args>
+  void construct( U* ptr, Args&& ... args ) {
+    a_t::construct(static_cast<A&>(*this), ptr, std::forward<Args>(args)...);
+  }
+};
+#endif
+
+//--------------------------------------------------------------------------
 using VectorUInt = std::vector<UInt_t>;
 // std::vector that does NOT zero-initialize its elements on resize()
 using VectorUIntNI = std::vector<UInt_t, default_init_allocator<UInt_t>>;
