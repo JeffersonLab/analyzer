JeffersonLab/analyzer

commit 5c7edc41a4adb1f0e7cf73e71e450df63eb87b82

CodaDecoder: Add multiblock support to trigBankDecode

hana_decode/CodaDecoder.cxx
@@ -341,53 +341,70 @@ UInt_t CodaDecoder::trigBankDecode( const UInt_t* evbuffer, UInt_t blkSize) {
 // Decode the "Trigger Bank" which is a CODA3 structure that appears
 // near the top of the event buffer.  
 
-  memset((void *)&tbank, 0, sizeof(TBOBJ));
+  memset(&tbank, 0, sizeof(TBOBJ));
 
-  tbank.start = (uint32_t *)evbuffer;
+  tsEvType = -1;
+  evt_time = 0;
+  trigger_bits = 0;
+
+  tbank.start = (uint32_t*) evbuffer;
   tbank.blksize = blkSize;
   tbank.len = evbuffer[0] + 1;
-  tbank.tag = (evbuffer[1]&0xffff0000)>>16;
-  tbank.nrocs = (evbuffer[1]&0xff);
- tsEvType = -1;
-  memcpy(&tbank.evtNum, evbuffer + 3, sizeof(uint64_t));
+  tbank.tag = (evbuffer[1] & 0xffff0000) >> 16;
+  tbank.nrocs = evbuffer[1] & 0xff;
 
-  if((tbank.tag)&1)
-    tbank.withTimeStamp = 1;
-  if((tbank.tag)&2)
-    tbank.withRunInfo = 1;
-
-  if(tbank.withTimeStamp) {
-    tbank.evTS = (uint64_t *)&evbuffer[5];
-    if(tbank.withRunInfo) {
-      tbank.evType = (uint16_t *)&evbuffer[5 + 2*blkSize + 3];
-    }else{
-      tbank.evType = (uint16_t *)&evbuffer[5 + 2*blkSize + 1];
+  uint32_t* p = (uint32_t*) evbuffer + 2;
+  // Segment 1:
+  //  uint64_t event_number
+  //  uint64_t run_info                if withRunInfo
+  //  uint64_t time_stamp[blkSize]     if withTimeStamp
+  {
+    uint32_t len = *p & 0xffff;
+    auto* q = (uint64_t*) (p + 1);
+    tbank.evtNum = *(q++);
+    if( tbank.withRunInfo() )
+      q++;  // TODO: extract run info
+    if( tbank.withTimeStamp() ) {
+      tbank.evTS = q;
+      evt_time = tbank.evTS[0];  // time of first event in block
     }
-  }else{
-    tbank.evTS = nullptr;
-    if(tbank.withRunInfo) {
-      tbank.evType = (uint16_t *)&evbuffer[5 + 3];
-    }else{
-      tbank.evType = (uint16_t *)&evbuffer[5 + 1];
-    }
+    p += len + 1;
   }
-  
- tsEvType = tbank.evType[0];
+  assert( p-evbuffer < tbank.len);
 
-/* Search for TS segment.  */
-  evt_time=0;
-  trigger_bits=0;
-  for (UInt_t i=5; i<tbank.len; i++) {
-    Int_t rocnum=(evbuffer[i]&0xff000000)>>24;
-    if (rocnum==fgTSROC) {
-        evt_time = evbuffer[i+1];
-        trigger_bits = evbuffer[i+3]; 
-    }
+  // Segment 2:
+  //  uint16_t event_type[blkSize]
+  //  padded to next 32-bit boundary
+  {
+    uint32_t len = *p & 0xffff;
+    tbank.evType = (uint16_t*) (p + 1);
+    tsEvType = tbank.evType[0];  // type of first event in block
+    p += len + 1;
   }
 
-  
-  return(tbank.len);
+  // tbank.nroc ROC segments containing timestamps and optional
+  // data like trigger latch bits:
+  // struct {
+  //   uint64_t roc_time_stamp;
+  //   uint32_t roc_trigger_bits;   // this is optional!
+  // } roc_segment[blkSize];
+  for( uint32_t i = 0; i < tbank.nrocs; ++i ) {
+    assert( p-evbuffer < tbank.len);
+    uint32_t len = *p & 0xffff;
+    uint32_t rocnum = (*p & 0xff000000) >> 24;
+    if( rocnum == fgTSROC ) {
+      tbank.TSROC = p + 1;
+      tbank.tsrocLen = len;
+      if( !tbank.withTimeStamp() )
+        evt_time = *(uint64_t*) tbank.TSROC;
+      if( len > 2 )
+        trigger_bits = tbank.TSROC[2];  // trigger bits of first event in block
+      break;
+    }
+    p += len + 1;
+  }
 
+  return tbank.len;
 }
 
 //_____________________________________________________________________________

hana_decode/CodaDecoder.h
@@ -89,19 +89,22 @@ protected:
 
   class TBOBJ {
   public:
-     TBOBJ() : blksize(0), tag(0), nrocs(0), len(0), withTimeStamp(0),
-         withRunInfo(0), evtNum(0), runInfo(0), start(nullptr), evTS(nullptr), evType(nullptr) {}
+     TBOBJ() : blksize(0), tag(0), nrocs(0), len(0), evtNum(0), runInfo(0),
+               start(nullptr), evTS(nullptr), evType(nullptr), TSROC(nullptr),
+               tsrocLen(0) {}
+     bool withTimeStamp() const { return (tag & 1) != 0; }
+     bool withRunInfo()   const { return (tag & 2) != 0; }
      uint32_t blksize;              /* total number of triggers in the Bank */
      uint16_t tag;                  /* Trigger Bank Tag ID = 0xff2x */
      uint16_t nrocs;                /* Number of ROC Banks in the Event Block (val = 1-256) */
      uint32_t len;                  /* Total Length of the Trigger Bank - including Bank header */
-     int      withTimeStamp;        /* =1 if Time Stamps are available */
-     int      withRunInfo;          /* =1 if Run information is available - Run # and Run Type */
      uint64_t evtNum;               /* Starting Event # of the Block */
      uint64_t runInfo;              /* Run Info Data */
      uint32_t *start;               /* Pointer to start of the Trigger Bank */
      uint64_t *evTS;                /* Pointer to the array of Time Stamps */
      uint16_t *evType;              /* Pointer to the array of Event Types */
+     uint32_t *TSROC;               /* Pointer to Trigger Supervisor ROC segment data */
+     uint32_t tsrocLen;             /* Number of elements in TSROC array */
    };
 
   TBOBJ tbank;

commit 5f1e3e9db314f74b008c378cdf80e5151bc4f462

CodaDecoder: Swap order of time_stamp[] and run_info in trigger bank
This is a regression introduced in commit 5c7edc41 (28-Oct-21), where
the run info word was mistakenly assumed to precede the time_stamp[]
array. Since run info is not enabled by default in CODA, this bug does
not affect any SBS and NPS data to the best of my knowledge.

hana_decode/CodaDecoder.cxx
@@ -785,28 +785,31 @@ void CodaDecoder::PrintBankInfo() const
 uint32_t CodaDecoder::TBOBJ::Fill( const uint32_t* evbuffer,
                                    uint32_t blkSize, uint32_t tsroc )
 {
+  // For the data format, see the "Physics Event's Built Trigger Bank" diagram
+  // in https://coda.jlab.org/drupal/system/files/eventbuilding.pdf
+
   if( blkSize == 0 )
     throw std::invalid_argument("CODA block size must be > 0");
   start = evbuffer;
   blksize = blkSize;
   len = evbuffer[0] + 1;
-  tag = (evbuffer[1] & 0xffff0000) >> 16;
+  tag = evbuffer[1] >> 16;
   nrocs = evbuffer[1] & 0xff;
   runInfo = 0;
 
   const uint32_t* p = evbuffer + 2;
   // Segment 1:
   //  uint64_t event_number
-  //  uint64_t run_info                if withRunInfo
   //  uint64_t time_stamp[blkSize]     if withTimeStamp
+  //  uint64_t run_info                if withRunInfo
   {
     uint32_t slen = *p & 0xffff;  // segment length in 32-bit words
-    if( slen != 2*(1 + (withRunInfo() ? 1 : 0) + (withTimeStamp() ? blkSize : 0)))
+    if( slen != 2*(1 + withTimeStamp() * blkSize + withRunInfo()) )
       throw coda_format_error("Invalid length for Trigger Bank seg 1");
     memcpy(&evtNum, p + 1, sizeof(evtNum));
+    evTS = withTimeStamp() ? p + 3 : nullptr;
     if( withRunInfo() )
-      memcpy(&runInfo, p + 3, sizeof(runInfo));
-    evTS = withTimeStamp() ? p + 3 + (withRunInfo() ? 2 : 0) : nullptr;
+      memcpy(&runInfo, p + 3 + withTimeStamp() * 2UL * blksize, sizeof(runInfo));
     p += slen + 1;
   }
   if( p-evbuffer >= len )
@@ -835,7 +838,7 @@ uint32_t CodaDecoder::TBOBJ::Fill( const uint32_t* evbuffer,
     if( p-evbuffer >= len )
       throw coda_format_error("Past end of bank while scanning trigger bank segments");
     uint32_t slen = *p & 0xffff;
-    uint32_t rocnum = (*p & 0xff000000) >> 24;
+    uint32_t rocnum = *p >> 24;
     if( rocnum == tsroc ) {
       TSROC = p + 1;
       tsrocLen = slen;