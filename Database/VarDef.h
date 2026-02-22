f48f9f7220dc5507133ed807eeb8ff40791d7751
Ole Hansen <ole@jlab.org>
2026-02-22 14:22:55 -0500

    VarDef: Add default initializers

--- Database/VarDef.h
+++ Database/VarDef.h
@@ -16,28 +16,28 @@
 #include "VarType.h"  // IWYU pragma: export
 
 struct VarDef {
-  const char*      name;     // Variable name
-  const char*      desc;     // Variable description
-  Int_t            type;     // Variable data type (see VarType.h)
-  Int_t            size;     // Size of array (0/1 = scalar)
-  const void*      loc;      // Location of data
-  const Int_t*     count;    // Optional: Actual size of variable size array
+  const char*  name     {nullptr};   // Variable name
+  const char*  desc     {""};        // Variable description
+  Int_t        type     {kDouble};   // Variable data type (see VarType.h)
+  Int_t        size     {0};         // Size of array (0/1 = scalar)
+  const void*  loc      {nullptr};   // Location of data
+  const Int_t* count    {nullptr};   // (opt) Actual size of variable size array
 };
 
 struct RVarDef {
-  const char*      name;     // Variable name
-  const char*      desc;     // Variable description
-  const char*      def;      // Definition of data (data member or method name)
+  const char*  name     {nullptr};   // Variable name
+  const char*  desc     {""};        // Variable description
+  const char*  def      {nullptr};   // Definition of data (data member or method name)
 };
 
 struct DBRequest {
-  const char*      name;     // Key name
-  void*            var;      // Pointer to data (default to Double*)
-  VarType          type;     // (opt) data type (see VarType.h, default Double_t)
-  UInt_t           nelem;    // (opt) number of array elements (0/1 = 1 or auto)
-  Bool_t           optional; // (opt) If true, missing key is ok
-  Int_t            search;   // (opt) Search for key along name tree
-  const char*      descript; // (opt) Key description (if 0, same as name)
+  const char*  name     {nullptr};   // Key name
+  void*        var      {nullptr};   // Pointer to data (default to Double*)
+  VarType      type     {kDouble};   // (opt) data type (see VarType.h, default Double_t)
+  UInt_t       nelem    {0};         // (opt) number of array elements (0/1 = 1 or auto)
+  Bool_t       optional {false};     // (opt) If true, missing key is ok
+  Int_t        search   {0};         // (opt) Search for key along name tree
+  const char*  descript {nullptr};   // (opt) Key description (if 0, same as name)
 };
 
 #endif
