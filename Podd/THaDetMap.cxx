JeffersonLab/analyzer

commit cd1cf35c129591a4da42d75a91f2bde942c2c55c

Refactor codebase to UInt_t data type where applicable
Rules of thumb:
The following are unsigned int (UInt_t) by default:
- Raw data words
- Data directly extracted from raw data words
- Buffer positions, offsets and indices
- Hex constants
- "Mask" variables
- Crate, slot, channel and hit numbers
- Event numbers
- "Number of" counters
- "Size of" and "length" variables

The following are kept as signed int (Int_t):
- "Mode" variables
- Module numbers (these are negative in some applications)
- Bank numbers (might want to change to UInt_t too)
- Status return codes

Podd/THaDetMap.cxx
@@ -128,9 +128,9 @@ THaDetMap& THaDetMap::operator=( const THaDetMap& rhs )
 }
 
 //_____________________________________________________________________________
-Int_t THaDetMap::AddModule( Int_t crate, Int_t slot,
-                            Int_t chan_lo, Int_t chan_hi,
-                            Int_t first, Int_t model, Int_t refindex,
+Int_t THaDetMap::AddModule( UInt_t crate, UInt_t slot,
+                            UInt_t chan_lo, UInt_t chan_hi,
+                            UInt_t first, Int_t model, Int_t refindex,
                             Int_t refchan, Int_t plane, Int_t signal )
 {
   // Add a module to the map.
@@ -188,12 +188,12 @@ Int_t THaDetMap::AddModule( Int_t crate, Int_t slot,
     m.type = ChannelType::kUndefined;
 
   fMap.push_back(std::move(pm));
-  return GetSize();
+  return static_cast<Int_t>(GetSize());
 }
 
 //_____________________________________________________________________________
-THaDetMap::Module* THaDetMap::Find( Int_t crate, Int_t slot,
-                                    Int_t chan )
+THaDetMap::Module* THaDetMap::Find( UInt_t crate, UInt_t slot,
+                                    UInt_t chan )
 {
   // Return the module containing crate, slot, and channel chan.
   // If several matching modules exist (which mean the map is misconfigured),
@@ -264,10 +264,10 @@ Int_t THaDetMap::Fill( const vector<Int_t>& values, UInt_t flags )
     tuple_size++;
 
   // For historical reasons, logical channel numbers start at 1
-  Int_t prev_first = 1, prev_nchan = 0;
+  UInt_t prev_first = 1, prev_nchan = 0;
   // Defaults for optional values
-  Int_t first = 0, plane = 0, signal = 0;
-  Int_t model = 0, rchan = -1, ref = -1;
+  UInt_t first = 0;
+  Int_t  plane = 0, signal = 0, model = 0, rchan = -1, ref = -1;
 
   Int_t ret = 0;
   for( vsiz_t i = 0; i < values.size(); i += tuple_size ) {
@@ -304,7 +304,11 @@ Int_t THaDetMap::Fill( const vector<Int_t>& values, UInt_t flags )
     if( flags & kFillSignal )
       signal = values[i + k++];
 
-    ret = AddModule(values[i], values[i + 1], values[i + 2], values[i + 3],
+    auto crate = static_cast<UInt_t>(values[i]);
+    auto slot  = static_cast<UInt_t>(values[i+1]);
+    auto ch_lo = static_cast<UInt_t>(values[i+2]);
+    auto ch_hi = static_cast<UInt_t>(values[i+3]);
+    ret = AddModule(crate, slot, ch_lo, ch_hi,
                     first, model, ref, rchan, plane, signal);
     if( ret <= 0 )
       break;
@@ -316,12 +320,12 @@ Int_t THaDetMap::Fill( const vector<Int_t>& values, UInt_t flags )
 }
 
 //_____________________________________________________________________________
-Int_t THaDetMap::GetTotNumChan() const
+UInt_t THaDetMap::GetTotNumChan() const
 {
   // Get sum of the number of channels of all modules in the map. This is
   // typically the total number of hardware channels used by the detector.
 
-  Int_t sum = 0;
+  UInt_t sum = 0;
   for( const auto & m : fMap )
     sum += m->GetNchan();
 
@@ -330,7 +334,7 @@ Int_t THaDetMap::GetTotNumChan() const
 
 
 //_____________________________________________________________________________
-void THaDetMap::GetMinMaxChan( Int_t& min, Int_t& max, ECountMode mode ) const
+void THaDetMap::GetMinMaxChan( UInt_t& min, UInt_t& max, ECountMode mode ) const
 {
   // Put the minimum and maximum logical or reference channel numbers
   // into min and max. If refidx is true, check refindex, else check logical
@@ -340,8 +344,8 @@ void THaDetMap::GetMinMaxChan( Int_t& min, Int_t& max, ECountMode mode ) const
   max = kMinInt;
   bool do_ref = (mode == kRefIndex);
   for( const auto& m : fMap ) {
-    Int_t m_min = do_ref ? m->refindex : m->first;
-    Int_t m_max = do_ref ? m->refindex : m->first + m->hi - m->lo;
+    UInt_t m_min = do_ref ? m->refindex : m->first;
+    UInt_t m_max = do_ref ? m->refindex : m->first + m->hi - m->lo;
     if( m_min < min )
       min = m_min;
     if( m_max > max )
@@ -442,8 +446,8 @@ string THaDetMap::Iterator::msg( const char* txt ) const
 }
 
 #define CRATE_SLOT( x ) (x).crate, (x).slot
-static inline bool LOOPDONE( Int_t i, Int_t n ) { return i < 0 or i >= n; }
-static inline bool NO_NEXT( Int_t& i, Int_t n ) { return i >= n or ++i >= n; }
+static inline bool LOOPDONE( Int_t i, UInt_t n ) { return i < 0 or static_cast<UInt_t>(i) >= n; }
+static inline bool NO_NEXT( Int_t& i, UInt_t n ) { return i >= static_cast<Int_t>(n) or ++i >= static_cast<Int_t>(n); }
 
 //_____________________________________________________________________________
 THaDetMap::Iterator& THaDetMap::Iterator::operator++()
@@ -467,10 +471,10 @@ THaDetMap::Iterator& THaDetMap::Iterator::operator++()
  nextchan:
   if( NO_NEXT(fIChan, fNChan) )
     goto nextmod;
-  Int_t chan = fEvData.GetNextChan(CRATE_SLOT(fHitInfo), fIChan);
+  UInt_t chan = fEvData.GetNextChan(CRATE_SLOT(fHitInfo), fIChan);
   if( chan < fMod->lo or chan > fMod->hi )
     goto nextchan;  // Not one of my channels
-  Int_t nhit = fEvData.GetNumHits(CRATE_SLOT(fHitInfo), chan);
+  UInt_t nhit = fEvData.GetNumHits(CRATE_SLOT(fHitInfo), chan);
   fHitInfo.chan = chan;
   fHitInfo.nhit = nhit;
   if( nhit == 0 ) {
@@ -488,7 +492,7 @@ THaDetMap::Iterator& THaDetMap::Iterator::operator++()
   Int_t lchan = fMod->ConvertToLogicalChannel(chan);
   if( fDetMap.fStartAtZero )
     ++lchan; // ConvertToLogicalChannel subtracts 1; undo that here
-  if( lchan < 0 or lchan >= size() ) {
+  if( lchan < 0 or static_cast<UInt_t>(lchan) >= size() ) {
     ostringstream ostr;
     size_t lmin = 1, lmax = size(); // Apparent range in the database file
     if( fDetMap.fStartAtZero ) { --lmin; --lmax; }

Podd/THaDetMap.h
@@ -43,11 +43,11 @@ public:
     Module( Module& rhs ) = default;
     Module& operator=( Module& rhs ) = default;
 #endif
-    Int_t    crate;
-    Int_t    slot;
-    Int_t    lo;
-    Int_t    hi;
-    Int_t    first;      // logical number of first channel
+    UInt_t   crate;
+    UInt_t   slot;
+    UInt_t   lo;
+    UInt_t   hi;
+    UInt_t   first;      // logical number of first channel
     Int_t    model;      // model number of module (for ADC/TDC identification).
     Decoder::ChannelType type;
     Int_t    plane;      // Detector plane
@@ -62,7 +62,7 @@ public:
     { return crate == rhs.crate && slot == rhs.slot; }
     bool   operator!=( const Module& rhs ) const
     { return !(*this==rhs); }
-    Int_t  GetNchan() const { return hi-lo+1; }
+    UInt_t GetNchan() const { return hi-lo+1; }
     Int_t  GetModel() const { return model; }
     Bool_t IsTDC()    const { return
         type == Decoder::ChannelType::kCommonStopTDC ||
@@ -80,8 +80,8 @@ public:
     void   MakeADC();
     // Get logical detector channel from physical one, starting at 0.
     // For historical reasons, 'first' starts at 1, so subtract 1 here.
-    Int_t  ConvertToLogicalChannel( Int_t chan ) const
-    { return first + (reverse ? hi - chan : chan - lo) - 1; }
+    Int_t  ConvertToLogicalChannel( UInt_t chan ) const
+    { return static_cast<Int_t>(first + (reverse ? hi-chan : chan-lo) - 1); }
   };
 
   // Flags for GetMinMaxChan()
@@ -108,20 +108,20 @@ public:
   THaDetMap::Iterator MakeIterator( const THaEvData& evdata );
   THaDetMap::MultiHitIterator MakeMultiHitIterator( const THaEvData& evdata );
 
-  virtual Int_t     AddModule( Int_t crate, Int_t slot,
-                               Int_t chan_lo, Int_t chan_hi,
-                               Int_t first= 0, Int_t model= 0,
-                               Int_t refindex= -1, Int_t refchan= -1,
-                               Int_t plane= 0, Int_t signal= 0 );
+  virtual Int_t     AddModule( UInt_t crate, UInt_t slot,
+                               UInt_t chan_lo, UInt_t chan_hi,
+                               UInt_t first = 0, Int_t model = 0,
+                               Int_t refindex = -1, Int_t refchan = -1,
+                               Int_t plane = 0, Int_t signal = 0 );
           void      Clear()  { fMap.clear(); }
-  virtual Module*   Find( Int_t crate, Int_t slot, Int_t chan );
+  virtual Module*   Find( UInt_t crate, UInt_t slot, UInt_t chan );
   virtual Int_t     Fill( const std::vector<Int_t>& values, UInt_t flags = 0 );
-          void      GetMinMaxChan( Int_t& min, Int_t& max,
-				   ECountMode mode = kLogicalChan ) const;
+          void      GetMinMaxChan( UInt_t& min, UInt_t& max,
+                                   ECountMode mode = kLogicalChan ) const;
           Module*   GetModule( UInt_t i ) const;
-          Int_t     GetNchan( UInt_t i ) const;
-          Int_t     GetTotNumChan() const;
-          Int_t     GetSize() const { return static_cast<Int_t>(fMap.size()); }
+          UInt_t    GetNchan( UInt_t i ) const;
+          UInt_t    GetTotNumChan() const;
+          UInt_t    GetSize() const { return fMap.size(); }
 
           Int_t     GetModel( UInt_t i ) const;
           Bool_t    IsADC( UInt_t i ) const;
@@ -161,20 +161,22 @@ public:
       Iterator clone(*this); ++(*this); return clone;
     }
     virtual void reset();
-    Int_t size() const { return fNTotChan; }
+    UInt_t size() const { return fNTotChan; }
 
     // Status
     explicit virtual operator bool() const {
-      return fIMod>=0 and fIMod<fNMod and fIChan>=0 and fIChan<fNChan;
+      return (fIMod >= 0 and fIMod < static_cast<Int_t>(fNMod) and
+              fIChan >= 0 and fIChan < static_cast<Int_t>(fNChan));
     }
     bool operator!() const { return !static_cast<bool>(*this); }
 
     // Current value
     class HitInfo_t {
     public:
-      HitInfo_t()
-        : module{nullptr}, type{Decoder::ChannelType::kUndefined}, ev{-1},
-          crate{-1}, slot{-1}, chan{-1}, nhit{0}, hit{-1}, lchan{-1} {}
+      HitInfo_t() :
+        module{nullptr}, type{Decoder::ChannelType::kUndefined},
+        ev{kMaxUInt}, crate{kMaxUInt}, slot{kMaxUInt}, chan{kMaxUInt}, nhit{0},
+        hit{kMaxUInt}, lchan{-1} {}
       void set_crate_slot( const THaDetMap::Module* mod ) {
         if( mod->IsADC() )
           type = Decoder::ChannelType::kADC;
@@ -186,17 +188,17 @@ public:
       }
       void reset() {
         module = nullptr; type = Decoder::ChannelType::kUndefined;
-        crate = -1; slot = -1; chan = -1; nhit = 0; hit = -1; lchan = -1;
+        crate = slot = chan = hit = kMaxUInt; nhit = 0; lchan = -1;
       }
       Decoder::Module*     module; // Current frontend module being decoded
       Decoder::ChannelType type;   // Type of measurement recorded in current channel
-      Int_t  ev;      // Event number (for error messages)
-      Int_t  crate;   // Hardware crate
-      Int_t  slot;    // Hardware slot
-      Int_t  chan;    // Physical channel in current module
-      Int_t  nhit;    // Number of hits in current channel
-      Int_t  hit;     // Hit number in current channel
-      Int_t  lchan;   // Logical channel according to detector map
+      UInt_t  ev;      // Event number (for error messages)
+      UInt_t  crate;   // Hardware crate
+      UInt_t  slot;    // Hardware slot
+      UInt_t  chan;    // Physical channel in current module
+      UInt_t  nhit;    // Number of hits in current channel
+      UInt_t  hit;     // Hit number in current channel
+      Int_t   lchan;   // Logical channel according to detector map
     };
 
     const HitInfo_t* operator->() const { return &fHitInfo; }
@@ -206,12 +208,12 @@ public:
     THaDetMap& fDetMap;
     const THaEvData& fEvData;
     const THaDetMap::Module* fMod;
-    Int_t fNMod;         // Number of modules in detector map (cached)
-    Int_t fNTotChan;     // Total number of detector map channels (cached)
-    Int_t fNChan;        // Number of channels active in current module
-    Int_t fIMod;         // Current module index in detector map
-    Int_t fIChan;        // Current channel
-    HitInfo_t fHitInfo;  // Current state of this iterator
+    UInt_t fNMod;         // Number of modules in detector map (cached)
+    UInt_t fNTotChan;     // Total number of detector map channels (cached)
+    UInt_t fNChan;        // Number of channels active in current module
+    Int_t  fIMod;         // Current module index in detector map
+    Int_t  fIChan;        // Current channel
+    HitInfo_t fHitInfo;   // Current state of this iterator
 
     // Formatter for exception messages
     std::string msg( const char* txt ) const;
@@ -224,7 +226,8 @@ public:
     MultiHitIterator() = delete;
     virtual ~MultiHitIterator() = default;
     explicit virtual operator bool() const {
-      return Iterator::operator bool() and fIHit>=0 and fIHit<fHitInfo.nhit;
+      return (Iterator::operator bool() and fIHit >= 0 and
+              fIHit < static_cast<Int_t>(fHitInfo.nhit));
     }
     virtual Iterator& operator++();
     virtual void reset();
@@ -272,7 +275,7 @@ inline Int_t THaDetMap::GetModel( UInt_t i ) const {
   return uGetModule(i)->GetModel();
 }
 
-inline Int_t THaDetMap::GetNchan( UInt_t i ) const {
+inline UInt_t THaDetMap::GetNchan( UInt_t i ) const {
   // Return the number of channels of the i-th module
   if( i >= fMap.size() ) return 0;
   return uGetModule(i)->GetNchan();

commit e378fc2f61ae5659a8fcdaa704530360f547b4b9

DetMap: Fix broken GetMinMaxChan, which really needs Int_t results
Regression introduced with commit cd1cf35c (29-Apr-2021)

Podd/THaDetMap.cxx
@@ -530,7 +530,7 @@ UInt_t THaDetMap::GetTotNumChan() const
 
 
 //_____________________________________________________________________________
-void THaDetMap::GetMinMaxChan( UInt_t& min, UInt_t& max, ECountMode mode ) const
+void THaDetMap::GetMinMaxChan( Int_t& min, Int_t& max, ECountMode mode ) const
 {
   // Put the minimum and maximum logical or reference channel numbers
   // into min and max. If refidx is true, check refindex, else check logical
@@ -538,13 +538,21 @@ void THaDetMap::GetMinMaxChan( UInt_t& min, UInt_t& max, ECountMode mode ) const
 
   min = kMaxInt;
   max = kMinInt;
+  Int_t minposref = kMaxInt;
   bool do_ref = (mode == kRefIndex);
   for( const auto& m : fMap ) {
-    UInt_t m_min = do_ref ? m->refindex : m->first;
-    UInt_t m_max = do_ref ? m->refindex : m->first + m->hi - m->lo;
+    Int_t m_min = do_ref ? m->refindex : ToInt(m->first);
+    Int_t m_max = do_ref ? m->refindex : ToInt(m->first + m->hi - m->lo);
+    Int_t m_minpos = m->refindex >= 0 ? m->refindex : kMaxInt;
     min = std::min(m_min, min);
     max = std::max(m_max, max);
+    minposref = std::min(m_minpos, minposref);
   }
+  // If at least one refindex is valid (not negative), report the range
+  // of valid refindexes. Otherwise, both min and max < 0, and there are no
+  // valid refindexes.
+  if( do_ref && max >= 0 )
+    min = minposref;
 }
 
 

Podd/THaDetMap.h
@@ -114,7 +114,7 @@ public:
   Module* Find( UInt_t crate, UInt_t slot, UInt_t chan );
   Int_t   Fill( const std::vector<Int_t>& values, UInt_t flags = 0 );
   Int_t   Fill( std::string_view dbtxt, UInt_t flags = 0 );
-  void    GetMinMaxChan( UInt_t& min, UInt_t& max,
+  void    GetMinMaxChan( Int_t& min, Int_t& max,
                          ECountMode mode = kLogicalChan ) const;
   Module* GetModule( UInt_t i ) const;
   UInt_t  GetNchan( UInt_t i ) const;