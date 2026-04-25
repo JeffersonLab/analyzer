41d663fb759a108d9ad02d0ca90eb30990ba4dc4
Ole Hansen <ole@jlab.org>
2026-04-24 22:26:49 -0400

    GenScaler: Drop fDebugFile. Small efficiency tweaks. Slight reformatting

--- hana_decode/GenScaler.cxx
+++ hana_decode/GenScaler.cxx
@@ -6,11 +6,12 @@
 /////////////////////////////////////////////////////////////////////
 
 #include "GenScaler.h"
-#include "THaEvData.h"
-#include "TMath.h"
-#include <iostream>
-#include <cassert>
-#include "TError.h"
+#include "THaSlotData.h"  // for THaSlotData
+#include <cassert>        // for assert
+#include <fstream>        // for ofstream
+#include <iostream>       // for cout, cerr, endl, dec, hex, operator-, operator<<
+#include <limits>         // for numeric_limits
+#include <type_traits>    // for is_unsigned_v
 
 using namespace std;
 
@@ -18,23 +19,35 @@ namespace Decoder {
 
   const int DEFAULT_DELTAT = 4;
 
-  GenScaler::GenScaler( UInt_t crate, UInt_t slot) :
-    VmeModule(crate, slot),
-    fIsDecoded(false), fFirstTime(true), fDeltaT(0.0),
-    fClockChan(0), fNumChanMask(0), fNumChanShift(0),
-    fHasClock(false), fClockRate(0), fNormScaler(nullptr),
-    firsttime(true), firstwarn(true)
+  //___________________________________________________________________________
+  GenScaler::GenScaler( UInt_t crate, UInt_t slot )
+    : VmeModule(crate, slot)
+    , fIsDecoded(false)
+    , fFirstTime(true)
+    , fDeltaT(0.0)
+    , fClockChan(0)
+    , fNumChanMask(0)
+    , fNumChanShift(0)
+    , fHasClock(false)
+    , fClockRate(0)
+    , fNormScaler(nullptr)
+    , firsttime(true)
+    , firstwarn(true)
   {
     fWordsExpect = 32;
     fNumChan = 0;
   }
 
-  void GenScaler::Clear(Option_t* opt) {
+  //___________________________________________________________________________
+  void GenScaler::Clear( Option_t* opt ) {
     // Clear event-by-event data
     VmeModule::Clear(opt);
-    fIsDecoded=false;
+    // Do not clear fDataArray, fPrevArray, fRate here since they should
+    // carry over between events
+    fIsDecoded = false;
   }
 
+  //___________________________________________________________________________
   void GenScaler::GenInit()
   {
     fHasClock = false;
@@ -46,15 +59,13 @@ namespace Decoder {
     fNumChanMask = 0xff;
     fNumChanShift = 0;
     fDeltaT = DEFAULT_DELTAT;  // a default time interval between readings
-    fDataArray.resize(fWordsExpect);
-    fPrevData.resize(fWordsExpect);
-    fRate.resize(fWordsExpect);
-    fDataArray.assign(fDataArray.size(), 0);
-    fPrevData.assign(fPrevData.size(), 0);
-    fRate.assign(fRate.size(), 0.0);
+    fDataArray.assign(fWordsExpect, 0);
+    fPrevData.assign(fWordsExpect, 0);
+    fRate.assign(fWordsExpect, 0.0);
   }
 
-  void GenScaler::SetBank(Int_t bank) {
+  //___________________________________________________________________________
+  void GenScaler::SetBank( Int_t bank ) {
     /// Define scaler header format for modules in banks
     fBank = bank;
     if(fBank > 0) {
@@ -63,6 +74,7 @@ namespace Decoder {
     }
   }
 
+  //___________________________________________________________________________
   Int_t GenScaler::SetClock( Double_t deltaT, UInt_t clockchan, Double_t clockrate) {
     // Sets the clock for the time base
     // retcode:
@@ -75,135 +87,139 @@ namespace Decoder {
     fHasClock = false;
     if (clockrate > 0) {
       if (fNormScaler) {
-	cout << "GenScaler:: WARNING:  Declaring this object to have"<<endl;
-	cout << "   a clock even though this also has a normalization scaler ?"<<endl;
-	cout << "  This makes no sense. "<<endl;
-	retcode = -1;
+	cerr << "GenScaler:: WARNING:  Declaring this object to have"<<endl;
+	cerr << "   a clock even though this also has a normalization scaler ?"<<endl;
+	cerr << "  This makes no sense. "<<endl;
+	return -1;
       }
       fHasClock = true;
     }
     if( clockchan >= fWordsExpect ) {
-      cout << "GenScaler:: ERROR:  clock channel out of range "<<endl;
-      retcode = -1;
-    } else {
-      fClockChan = clockchan;
+      cerr << "GenScaler:: ERROR:  clock channel out of range "<<endl;
+      return -1;
     }
+    fClockChan = clockchan;
     fClockRate = clockrate;
-    if (retcode >= 0 && fClockRate > 0) retcode=1;
-    if (deltaT > 0) {
+    if( fClockRate > 0 )
+      retcode = 1;
+    if( deltaT > 0 ) {
       fDeltaT = deltaT;
-      if (fClockRate <= 0 && retcode>=0) retcode=2;
+      if( fClockRate <= 0 )
+        retcode = 2;
     } else {
-      cout << "GenScaler:: Warning: using default deltaT = "<<fDeltaT<<endl;
+      cerr << "GenScaler:: Warning: using default deltaT = " << fDeltaT << endl;
     }
-    if (retcode == 0) cout << "GetScaler:: Warning:: no deltaT nor clockrate define for this scaler "<<endl;
+    if( retcode == 0 )
+      cerr << "GetScaler:: Warning:: no deltaT nor clockrate defined for this scaler " << endl;
     return retcode;
   }
 
+  //___________________________________________________________________________
   void GenScaler::LoadNormScaler(GenScaler *scal) {
     if (fHasClock) {
-      cout << "GenScaler:: WARNING:  loading norm scaler although this"<<endl;
-      cout << "   object has a clock ?   This makes no sense. "<<endl;
+      cerr << "GenScaler:: WARNING:  loading norm scaler although this" << endl;
+      cerr << "   object has a clock ?   This makes no sense. " << endl;
     }
-    if ( !scal ) {
-      cout << "GenScaler:: ERROR:  attempting to load a non-existent "<<endl;
-      cout << "normalization scaler !" <<endl;
+    if( !scal ) {
+      cerr << "GenScaler:: ERROR:  attempting to load a non-existent " << endl;
+      cerr << "normalization scaler !" << endl;
       return;
     }
     fNormScaler = scal;
   }
 
+  //___________________________________________________________________________
   Int_t GenScaler::Decode(const UInt_t *evbuffer) {
-    Int_t doload=0;
-    Int_t nfound=1;
-    if (IsDecoded()) return nfound;
-    if (fFirstTime) {
+    Int_t doload = 0;
+    Int_t nfound = 1;
+    if( IsDecoded() )
+      return nfound;
+    if( fFirstTime ) {
       fFirstTime = false;
     } else {
-      doload=1;
-      fPrevData = fDataArray;
+      doload = 1;
+      fPrevData.swap(fDataArray);
     }
     //if ( !IsSlot(*evbuffer) ) return nfound; // redundant, checked in LoadSlot
     assert(IsSlot(*evbuffer));
-    if (fDebugFile) *fDebugFile << "is slot 0x"<<hex<<*evbuffer<<dec<<" num chan "<<fNumChan<<endl;
     evbuffer++;
     fIsDecoded = true;
-    fDataArray.assign( evbuffer, evbuffer+fNumChan );
+    fDataArray.assign(evbuffer, evbuffer + fNumChan);
     nfound += fNumChan;
-    if (fDebugFile) {
-      for( UInt_t i = 0; i < fNumChan; i++ )
-        *fDebugFile << "   data[" << i << "] = 0x" << hex << fDataArray[i] << dec << endl;
-    }
-    if (doload) LoadRates();
+    if( doload )
+      LoadRates();
     return nfound;
   }
 
-  Double_t GenScaler::GetTimeSincePrev() const {
+  //___________________________________________________________________________
+  namespace {
+  template<typename T> requires std::is_unsigned_v<T>
+  constexpr T DiffWithOverflow( T cur, T prev )
+  {
+    // Return difference between cur and prev scaler values (monotonically
+    // increasing numbers), where cur may have wrapped around due to unsigned
+    // integer overflow. The result is undefined if the actual difference is
+    // larger than numeric_limits<T>::max(). This case cannot be detected.
+    if( cur < prev ) {
+      prev = numeric_limits<T>::max() - (prev - 1);
+      return cur + prev;  //  by construction, this will not overflow
+    }
+    return cur - prev;
+  }
+  }
+
+  //___________________________________________________________________________
+  Double_t GenScaler::GetTimeSincePrev() const { // NOLINT(*-no-recursion)
     // Time since previous reading.
     // If a normalization scaler was defined, use its time base.
     // Otherwise, if this scaler has a clock, use it to get the time precisely.
     // Finally, if there is no clock, use fDeltaT as an approximate time.
 
-    if (fNormScaler) return fNormScaler->GetTimeSincePrev();
-    Double_t dtime = 0;
-    if (fDebugFile) {
-      *fDebugFile << "Into GetTimeSincePrev "<<endl;
-      if (IsDecoded()) *fDebugFile << "Is Decoded "<<endl;
-      if (fHasClock) *fDebugFile << "has Clock "<<endl;
-    }
-    if (IsDecoded() && fHasClock && fClockRate>0 && checkchan(fClockChan)) {
-      // Check for scaler overflow
-      UInt_t clockdif;
-      if(fDataArray[fClockChan] < fPrevData[fClockChan]) {
-	clockdif = (kMaxUInt-(fPrevData[fClockChan]-1))
-	  + fDataArray[fClockChan];
-      } else {
-	clockdif = fDataArray[fClockChan]-fPrevData[fClockChan];
-      }
-      dtime = clockdif/fClockRate;
-      if (fDebugFile) *fDebugFile << "GetTimeSincePrev  "<<fClockRate<<"   "<<fClockChan<<"   "<<dtime<<endl;
-    } else {
-      if (fDeltaT > 0) dtime = fDeltaT;   // default
+    if( fNormScaler ) //FIXME generalize. Can have norm scaler AND clock
+      return fNormScaler->GetTimeSincePrev();
+    Double_t dtime = 0; //FIXME -> division by zero in LoadRates()
+    const UInt_t ch = fClockChan;  // shorthand for readability
+    if( IsDecoded() && fHasClock && fClockRate > 0 && checkchan(ch) ) {
+      UInt_t clockdif = DiffWithOverflow(fDataArray[ch], fPrevData[ch]);
+      dtime = clockdif / fClockRate;
+    } else if( fDeltaT > 0. ) {
+      dtime = fDeltaT; // default
     }
     return dtime;
   }
 
+  //___________________________________________________________________________
   void GenScaler::LoadRates() {
-    if (IsDecoded()) {
+    if( IsDecoded() ) {
       Double_t dtime = GetTimeSincePrev();
-      if (dtime==0) {
-	fRate.assign(fRate.size(), 0);
-	return;
+      if( dtime == 0. ) {
+        fRate.assign(fWordsExpect, 0.);
+        return;
       }
       for( UInt_t i = 0; i < fWordsExpect; i++ ) {
-	// Check for scaler overflow
-	UInt_t diff;
-	if(fDataArray[i] < fPrevData[i]) {
-	  diff = (kMaxUInt-(fPrevData[i]-1)) + fDataArray[i];
-	} else {
-	  diff = fDataArray[i]-fPrevData[i];
-	}
-	fRate[i] = diff/dtime;
+        UInt_t diff = DiffWithOverflow(fDataArray[i], fPrevData[i]);
+        fRate[i] = diff / dtime;
       }
     }
   }
 
-  UInt_t GenScaler::GetData( UInt_t chan) const {
-    if (checkchan(chan)) {
+  //___________________________________________________________________________
+  UInt_t GenScaler::GetData( UInt_t chan ) const {
+    if( checkchan(chan) ) {
       return fDataArray[chan];
-    } else {
-      return 0;
     }
+    return 0;
   }
 
-  Double_t GenScaler::GetRate( UInt_t chan) const {
-    if (checkchan(chan)) {
+  //___________________________________________________________________________
+  Double_t GenScaler::GetRate( UInt_t chan ) const {
+    if( checkchan(chan) ) {
       return fRate[chan];
-    } else {
-      return 0;
     }
+    return 0;
   }
 
+  //___________________________________________________________________________
   void GenScaler::DoPrint() const {
     cout << "GenScaler::   crate "<<fCrate<<"   slot "<<fSlot<<endl;
     cout << "GenScaler::   Header 0x"<<hex<<fHeader<<"    Mask  0x"<<fHeaderMask<<dec<<endl;
@@ -215,7 +231,8 @@ namespace Decoder {
     cout << "Clock channel "<<fClockChan<<"   clock rate "<<fClockRate<<endl;
   }
 
-  void GenScaler::DebugPrint(ofstream *file) const {
+  //___________________________________________________________________________
+  void GenScaler::DebugPrint( ofstream* file ) const {
     if (!file) return;
     *file << "GenScaler::   crate "<<fCrate<<"   slot "<<fSlot<<endl;
     *file << "GenScaler::   Header 0x"<<hex<<fHeader<<"    Mask  0x"<<fHeaderMask<<dec<<endl;
@@ -230,6 +247,7 @@ namespace Decoder {
     }
   }
 
+  //___________________________________________________________________________
   Bool_t GenScaler::IsSlot( UInt_t rdata ) {
     /// Check if this word is the header for the slot we are looking for
     /// Get the number of channels in this module from the header and
@@ -266,7 +284,9 @@ namespace Decoder {
     return true;
   }
 
-UInt_t GenScaler::LoadSlot( THaSlotData* sldat, const UInt_t* evbuffer, const UInt_t* pstop )
+  //___________________________________________________________________________
+  UInt_t GenScaler::LoadSlot( THaSlotData* sldat, const UInt_t* evbuffer,
+                              const UInt_t* pstop )
   {
     // This is a simple, default method for loading a slot
     // pstop points to last word of data
@@ -279,28 +299,29 @@ UInt_t GenScaler::LoadSlot( THaSlotData* sldat, const UInt_t* evbuffer, const UI
     const UInt_t* p = evbuffer;
     while( p < pstop ) {
       if( IsSlot(*p) ) {   // Sets fNumChan
-	if (fDebugFile) *fDebugFile << "GenScaler:: Loadslot "<<endl;
         if( p + fNumChan > pstop ) {
           Error(here, "(%u,%u) Num channels %u points past end of buffer. "
-                      "Possibly unsupported data format. Ignoring data.",
+                "Possibly unsupported data format. Ignoring data.",
                 fCrate, fSlot, fNumChan);
           return 0;
         }
-	Decode(p);
+        Decode(p);
         if( sldat ) {
           for( UInt_t ichan = 0; ichan < fNumChan; ichan++ ) {
             sldat->loadData(ichan, fDataArray[ichan], fDataArray[ichan]);
           }
         }
-	fWordsSeen = fNumChan;
-	return fWordsSeen;
+        fWordsSeen = fNumChan;
+        return fWordsSeen;
       }
       p++;
     }
     return 0;
   }
 
-  UInt_t GenScaler::LoadSlot( THaSlotData *sldat, const UInt_t* evbuffer, UInt_t pos, UInt_t len) {
+  //___________________________________________________________________________
+  UInt_t GenScaler::LoadSlot( THaSlotData *sldat, const UInt_t* evbuffer,
+                              UInt_t pos, UInt_t len) {
     /// Fill data structures of this class, utilizing bank structure
     /// Read until out of data or until decode says that the slot is finished
     /// len = ndata in event, pos = word number for block header in event
@@ -318,13 +339,13 @@ UInt_t GenScaler::LoadSlot( THaSlotData* sldat, const UInt_t* evbuffer, const UI
     while(fWordsSeen < len) {
       UInt_t index = pos + fWordsSeen;
       if( IsSlot(evbuffer[index]) ) {  // Sets fNumChan
-        if( index + fNumChan >= pos+len ) {
+        if( index + fNumChan >= pos + len ) {
           Error(here, "(%u,%u) Num channels %u points past end of buffer. "
-                      "Possibly unsupported data format. Disabling module.",
+                "Possibly unsupported data format. Disabling module.",
                 fCrate, fSlot, fNumChan);
           return 0;
         }
-        fWordsSeen += Decode(evbuffer+index);
+        fWordsSeen += Decode(evbuffer + index);
         if( sldat ) {
           for( UInt_t ichan = 0; ichan < fNumChan; ichan++ ) {
             sldat->loadData(ichan, fDataArray[ichan], fDataArray[ichan]);
@@ -332,11 +353,12 @@ UInt_t GenScaler::LoadSlot( THaSlotData* sldat, const UInt_t* evbuffer, const UI
         }
         break;
       }
-      fWordsSeen += (fNumChan + 1); // Skip to next header
+      fWordsSeen += fNumChan + 1; // Skip to next header
     }
     return fWordsSeen;
   }
 
+  //___________________________________________________________________________
 }
 
 ClassImp(Decoder::GenScaler)
