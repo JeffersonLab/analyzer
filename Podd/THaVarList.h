53b003e5cc7083336e52a2301edcbbd2567856c1
Ole Hansen <ole@jlab.org>
2026-04-26 18:11:22 -0400

    THaVarList: Use templates for Define() functions
    A long-overdue cleanup of that messy API. The Cling interpreter, at
    least as of ROOT 6.36, has no problem instantiating these templates
    on the fly.

--- Podd/THaVarList.h
+++ Podd/THaVarList.h
@@ -18,153 +18,38 @@ public:
   THaVarList();
 
   // Define() with reference to variable
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Double_t& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kDouble, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Float_t& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kFloat, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Long64_t& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kLong, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const ULong64_t& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kULong, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Int_t& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kInt, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const UInt_t& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kUInt, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Short_t& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kShort, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const UShort_t& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kUShort, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Char_t& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kChar, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Byte_t& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kByte, count ); }
+  template<typename T>
+  THaVar* Define( const char* name, const char* descript, const T& var,
+                  const Int_t* count = nullptr )
+  {
+    auto type = Vars::FindType(typeid(T));
+    return DefineByType(name, descript, &var, type, count);
+  }
 
-  THaVar*  Define( const char* name, const char* descript, 
-  		   const std::vector<Int_t>& var )
-    { return DefineByType( name, descript, &var, kIntV, nullptr ); }
-  THaVar*  Define( const char* name, const char* descript, 
-  		   const std::vector<UInt_t>& var )
-    { return DefineByType( name, descript, &var, kUIntV, nullptr ); }
-  THaVar*  Define( const char* name, const char* descript, 
-  		   const std::vector<Float_t>& var )
-    { return DefineByType( name, descript, &var, kFloatV, nullptr ); }
-  THaVar*  Define( const char* name, const char* descript, 
-  		   const std::vector<Double_t>& var )
-    { return DefineByType( name, descript, &var, kDoubleV, nullptr ); }
-
-  THaVar*  Define( const char* name, const Double_t& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const Float_t& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const Long64_t& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const ULong64_t& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const Int_t& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const UInt_t& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const Short_t& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const UShort_t& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const Char_t& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const Byte_t& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-
-  THaVar*  Define( const char* name, const std::vector<Int_t>& var )
-    { return Define( name, name, var ); }
-  THaVar*  Define( const char* name, const std::vector<UInt_t>& var )
-    { return Define( name, name, var ); }
-  THaVar*  Define( const char* name, const std::vector<Float_t>& var )
-    { return Define( name, name, var ); }
-  THaVar*  Define( const char* name, const std::vector<Double_t>& var )
-    { return Define( name, name, var ); }
+  template<typename T>
+  THaVar* Define( const char* name, const T& var, const Int_t* count = nullptr )
+  { return Define(name, name, var, count); }
 
   // Define() with pointer to variable
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Double_t*& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kDoubleP, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Float_t*& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kFloatP, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Long64_t*& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kLongP, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const ULong64_t*& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kULongP, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Int_t*& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kIntP, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const UInt_t*& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kUIntP, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Short_t*& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kShortP, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const UShort_t*& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kUShortP, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Char_t*& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kCharP, count ); }
-  THaVar*  Define( const char* name, const char* descript, 
-		   const Byte_t*& var, const Int_t* count=nullptr )
-    { return DefineByType( name, descript, &var, kByteP, count ); }
+  template<typename T>
+  THaVar* Define( const char* name, const char* descript, const T* const& var,
+                  const Int_t* count = nullptr )
+  {
+    auto type = Vars::FindType(typeid(T));
+    return DefineByType(name, descript, &var, type, count);
+  }
 
-  THaVar*  Define( const char* name, const Double_t*& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const Float_t*& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const Long64_t*& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const ULong64_t*& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const Int_t*& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const UInt_t*& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const Short_t*& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
-  THaVar*  Define( const char* name, const UShort_t*& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
+  template<typename T>
+  THaVar* Define( const char* name, const T* const& var,
+                  const Int_t* count = nullptr )
+  { return Define(name, name, var, count); }
+
   //Avoid ambiguity - cannot specify variable length char array like this
   //Must use the form with description
-  THaVar*  Define( const char* name, const Char_t*& var )
-    { return Define( name, name, var ); }
-  THaVar*  Define( const char* name, const Byte_t*& var,
-		   const Int_t* count=nullptr )
-    { return Define( name, name, var, count ); }
+  THaVar* Define( const char* name, const Char_t* const& var )
+  {
+    return Define(name, name, var);
+  }
 
   virtual THaVar*  DefineByType( const char* name, const char* desc,
 				 const void* loc, VarType type,
